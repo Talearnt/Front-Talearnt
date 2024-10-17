bf4bff57f2a23d7c58688c0826051805045adde4
ChoiBooKi <qnrl9034@naver.com>
2024-10-17 18:21:23 +0900

    feat doing setting prettier TALEARNT-31

--- eslint.config.js
+++ eslint.config.js
@@ -1,87 +1,96 @@
-import eslint from '@eslint/js';
-import reactHooks from 'eslint-plugin-react-hooks'
-import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths"
-import tseslint from 'typescript-eslint'
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import importPlugin from "eslint-plugin-import";
+import reactHooks from "eslint-plugin-react-hooks";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
+import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
-export default tseslint.config(
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  importPlugin.flatConfigs.recommended,
   {
-    extends: [  eslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylistic
-    ],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       parser: tseslint.parser,
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+        tsconfigRootDir: import.meta.dirname
+      }
     },
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      "no-relative-import-paths":noRelativeImportPaths,
-      'react-hooks': reactHooks,
+      "@typescript-eslint": tseslint.plugin,
+      "no-relative-import-paths": noRelativeImportPaths,
+      "react-hooks": reactHooks,
+      "simple-import-sort": simpleImportSort
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/no-confusing-void-expression": "off",
-      'no-duplicate-imports': [
-        'error',
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "no-duplicate-imports": [
+        "error",
         {
-          includeExports: true,
-        },
+          includeExports: true
+        }
       ],
-      'object-shorthand': [
-        'error',
-        'always',
+      "object-shorthand": [
+        "error",
+        "always",
         {
-          avoidQuotes: true,
-        },
+          avoidQuotes: true
+        }
       ],
       "prefer-destructuring": [
         "error",
         {
-          "VariableDeclarator": {
-            "array": false,
-            "object": true
+          VariableDeclarator: {
+            array: false,
+            object: true
           },
-          "AssignmentExpression": {
-            "array": false,
-            "object": false
+          AssignmentExpression: {
+            array: false,
+            object: false
           }
         }
       ],
       "no-relative-import-paths/no-relative-import-paths": [
         "warn",
-        { "allowSameFolder": true, "rootDir": "src", "prefix": "@" }
+        { allowSameFolder: true, rootDir: "src", prefix: "@" }
       ],
       "@typescript-eslint/naming-convention": [
         "warn",
         {
-          "selector": "variable",
-          "format": ["strictCamelCase", "UPPER_CASE"]
+          selector: "variable",
+          format: ["strictCamelCase", "UPPER_CASE"]
         },
         {
-          "selector": "function",
-          "format": ["strictCamelCase", "PascalCase"]
+          selector: "function",
+          format: ["strictCamelCase", "PascalCase"]
         },
         {
-          "selector": "parameter",
-          "format": ["strictCamelCase"]
+          selector: "parameter",
+          format: ["strictCamelCase"]
         },
         {
-          "selector": "typeAlias",
-          "format": ["PascalCase"],
-          "suffix": ["Type", "Props"]
-        },
+          selector: "typeAlias",
+          format: ["PascalCase"],
+          suffix: ["Type", "Props"]
+        }
       ]
-    },
+    }
   },
   {
-    files: ['**/*.js'],
-    ...tseslint.configs.disableTypeChecked,
-  },
-);
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked
+  }
+];
