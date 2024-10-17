c179a4541a1886228d7ae13643ac202b46246323
ChoiBooKi <qnrl9034@naver.com>
2024-10-17 23:17:13 +0900

    feat setting simple-import-sort/imports TALEARNT-31

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,10 @@
 import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import importPlugin from "eslint-plugin-import";
-import reactHooks from "eslint-plugin-react-hooks";
 import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
-import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
 
 export default [
   eslint.configs.recommended,
@@ -14,7 +13,6 @@ export default [
   ...tseslint.configs.stylistic,
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
-  importPlugin.flatConfigs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
@@ -35,7 +33,36 @@ export default [
       ...reactHooks.configs.recommended.rules,
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/no-confusing-void-expression": "off",
-      "simple-import-sort/imports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // 1. React 관련 패키지
+            ["^react", "^@?\\w"],
+
+            // 2. 라이브러리
+            ["^\\w"], // 기본 패키지 (예: lodash, axios 등)
+
+            // 3. api 파일 (파일 명이 **.api로 시작)
+            ["^.+\\.api$"],
+
+            // 4. util 함수 (카멜케이스로 함수명, **.utils일 수도 있고 아닐 수도 있음)
+            ["^.+\\.(utils|[a-z]+[A-Z].*)$"], // .utils로 끝나거나 카멜케이스 함수명
+
+            // 5. custom hook (파일 명이 **.hook)
+            ["^.+\\.hook$"],
+
+            // 6. 컴포넌트 (components 폴더에서 가져올 것)
+            ["^components(/.*|$)"],
+
+            // 7. json이나 상수들
+            ["^.+\\.(json|constants)$"], // JSON 파일이나 상수 관련 파일
+
+            // 8. css/scss 파일
+            ["^.+\\.(css|scss)$"]
+          ]
+        }
+      ],
       "simple-import-sort/exports": "error",
       "no-duplicate-imports": [
         "error",
