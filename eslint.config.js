10d64bd9327fb77fea76bbd27f01e5afbd892310
ChoiBooKi <qnrl9034@naver.com>
2024-10-17 16:55:53 +0900

    feat add rules, extends TALEARNT-31

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,33 @@
-import js from '@eslint/js'
+import eslint from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks'
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths"
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'react-app',],
+    extends: [  eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylistic
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      "no-relative-import-paths":noRelativeImportPaths,
       'react-hooks': reactHooks,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-confusing-void-expression": "off",
       'no-duplicate-imports': [
         'error',
         {
@@ -28,6 +41,47 @@ export default tseslint.config(
           avoidQuotes: true,
         },
       ],
+      "prefer-destructuring": [
+        "error",
+        {
+          "VariableDeclarator": {
+            "array": false,
+            "object": true
+          },
+          "AssignmentExpression": {
+            "array": false,
+            "object": false
+          }
+        }
+      ],
+      "no-relative-import-paths/no-relative-import-paths": [
+        "warn",
+        { "allowSameFolder": true, "rootDir": "src", "prefix": "@" }
+      ],
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          "selector": "variable",
+          "format": ["strictCamelCase", "UPPER_CASE"]
+        },
+        {
+          "selector": "function",
+          "format": ["strictCamelCase", "PascalCase"]
+        },
+        {
+          "selector": "parameter",
+          "format": ["strictCamelCase"]
+        },
+        {
+          "selector": "typeAlias",
+          "format": ["PascalCase"],
+          "suffix": ["Type", "Props"]
+        },
+      ]
     },
   },
-)
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+);
