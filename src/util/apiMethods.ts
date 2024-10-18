dcb41637e2dd98f08f7686a5f6c107b832424367
ChoiBooKi <qnrl9034@naver.com>
2024-10-18 15:47:25 +0900

    feat make apiMethods - getAPI TALEARNT-30

--- src/util/apiMethods.ts
+++ src/util/apiMethods.ts
@@ -1,5 +1,7 @@
 import Axios from "axios";
 
+import { checkObjectType } from "./checkObjectType";
+
 const baseURL = "";
 const instance = Axios.create({
   baseURL,
@@ -7,3 +9,26 @@ const instance = Axios.create({
     Accept: "application/json"
   }
 });
+
+export const getAPI = async <T>(
+  url: string,
+  data?: Record<string, string | number | Record<string, unknown>>
+): Promise<T> => {
+  let queryParameter = "";
+
+  if (data) {
+    queryParameter = Object.keys(data)
+      .reduce(
+        (acc, cur) =>
+          `${acc}${cur}=${
+            checkObjectType(data[cur])
+              ? JSON.stringify(data[cur])
+              : String(data[cur])
+          }&`,
+        "?"
+      )
+      .slice(0, -1);
+  }
+
+  return (await instance.get<T>(`${url}${queryParameter}`)).data;
+};
