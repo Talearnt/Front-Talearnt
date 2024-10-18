779241184822ce48dab84f9d4c532ac295197f41
ChoiBooKi <qnrl9034@naver.com>
2024-10-18 15:59:51 +0900

    feat make apiMethods - patchAPI TALEARNT-30

--- src/util/apiMethods.ts
+++ src/util/apiMethods.ts
@@ -36,13 +36,14 @@ export const getAPI = async <T>(
 export const postAPI = async <T>(
   url: string,
   data?: Record<string, string | number | Record<string, unknown> | unknown[]>
-): Promise<T> => {
-  return (await instance.post<T>(url, data)).data;
-};
+): Promise<T> => (await instance.post<T>(url, data)).data;
 
 export const putAPI = async <T>(
   url: string,
   data?: Record<string, string | number | Record<string, unknown> | unknown[]>
-): Promise<T> => {
-  return (await Axios.put<T>(url, data)).data;
-};
+): Promise<T> => (await instance.put<T>(url, data)).data;
+
+export const patchAPI = async <T>(
+  url: string,
+  data?: Record<string, string | number | Record<string, unknown> | unknown[]>
+): Promise<T> => (await instance.patch<T>(url, data)).data;
