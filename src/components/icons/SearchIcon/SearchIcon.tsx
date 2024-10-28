51bb2cf5804fcc186f00db97425e7702a154afed
ChoiBooKi <qnrl9034@naver.com>
2024-10-28 23:18:29 +0900

    [FEAT] Make SearchIcon.tsx TALEARNT-116

---
+++ src/components/icons/SearchIcon/SearchIcon.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps } from "react";
+
+import { classNames } from "@utils/classNames";
+
+function SearchIcon({
+  className = "stroke-talearnt-primary",
+  ...props
+}: ComponentProps<"svg">) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={classNames(className)}
+      {...props}
+    >
+      <path
+        d="M17.3269 17.44L20.8 20.8M19.68 11.84C19.68 16.1699 16.1699 19.68 11.84 19.68C7.51009 19.68 4 16.1699 4 11.84C4 7.51009 7.51009 4 11.84 4C16.1699 4 19.68 7.51009 19.68 11.84Z"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
+
+export { SearchIcon };
