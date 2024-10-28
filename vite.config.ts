047e29b4510069805ae552b61677c04ddf8c1a3e
ChoiBooKi <qnrl9034@naver.com>
2024-10-28 21:17:34 +0900

    [FEAT] Apply vite-tsconfig-paths TALEARNT-108

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths/dist";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tsconfigPaths()]
 });
