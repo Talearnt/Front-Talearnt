115466aac0c12c33bcd1057997bc25f89c62de17
ChoiBooKi <qnrl9034@naver.com>
2024-10-29 14:58:55 +0900

    [FEAT] Apply CommonIconProps type TALEARNT-116

--- src/components/icons/CaretIcon/CaretIcon.tsx
+++ src/components/icons/CaretIcon/CaretIcon.tsx
@@ -1,17 +1,15 @@
-import { ComponentProps } from "react";
-
 import { cva } from "class-variance-authority";
 
 import { classNames } from "@utils/classNames";
 
-import { CustomVariantProps } from "@/common/common.type";
+import { CommonIconProps, CustomVariantProps } from "@/common/common.type";
 
 type CaretIconVariantsType = Record<
   "direction",
   Record<"top" | "right" | "bottom" | "left", string>
 >;
 
-type CaretIconProps = ComponentProps<"svg"> &
+type CaretIconProps = CommonIconProps &
   CustomVariantProps<CaretIconVariantsType>;
 
 const caretIconVariants = cva<CaretIconVariantsType>("", {
@@ -32,22 +30,27 @@ const caretIconVariants = cva<CaretIconVariantsType>("", {
  * 꼬리 없는 화살표 아이콘
  * 색 변경은 className에 "stroke-talearnt-gray-500" 와 같이 넣어주면 됨
  * direction은 화살표가 가르킬 방향
+ * size 변경은 scale에 배수를 넣어주면 됨
  *
  * @param {string | undefined} className
  * @param {"top" | "right" | "bottom" | "left"} direction
- * @param {Omit<CaretIconProps, "className" | "direction">} props
+ * @param {number | undefined} scale
+ * @param {Omit<CaretIconProps, "scale" | "className" | "direction">} props
  * @returns {JSX.Element}
  * @constructor
  */
 function CaretIcon({
   className = "stroke-talearnt-gray-500",
   direction,
+  scale,
   ...props
 }: CaretIconProps) {
+  const size = scale * 24;
+
   return (
     <svg
-      width="24"
-      height="24"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
