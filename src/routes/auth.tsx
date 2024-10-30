2d6a3df6963f500b458b55c9ae62e8c5d7121b15
ChoiBooKi <qnrl9034@naver.com>
2024-10-30 23:19:23 +0900

    [CHORE] login -> signin TALEARNT-101

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,11 +1,11 @@
 import { RouteObject } from "react-router-dom";
 
-import Login from "@pages/auth/Login/Login";
+import SignIn from "@pages/auth/SignIn/SignIn";
 
 const authRouter: RouteObject[] = [
   {
-    element: <Login />,
-    path: "login"
+    element: <SignIn />,
+    path: "sign-in"
   }
 ];
 
