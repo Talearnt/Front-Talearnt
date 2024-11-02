6e55b4687ca72b47424aa5be99720cbbc80de946
ChoiBooKi <qnrl9034@naver.com>
2024-11-02 20:16:19 +0900

    [FEAT] Apply Input.tsx TALEARNT-101

--- src/pages/auth/SignIn/SignIn.tsx
+++ src/pages/auth/SignIn/SignIn.tsx
@@ -1,7 +1,21 @@
+import { Input } from "@components/Input/Input";
+
 function SignIn() {
   return (
-    <div className={"flex flex-col"}>
-      <h1>로그인</h1>
+    <div className={"mx-auto mt-[6.5rem] flex w-[26rem] flex-col items-center"}>
+      <h1 className={"mb-10 text-[1.875rem]"}>로그인</h1>
+      <Input
+        id={"id-input"}
+        label={"아이디"}
+        placeholder={"이메일을 입력해 주세요."}
+        wrapperClassName={"mt-6"}
+      />
+      <Input
+        id={"pw-input"}
+        label={"비밀번호"}
+        placeholder={"비밀번호를 입력해 주세요."}
+        wrapperClassName={"mt-4"}
+      />
     </div>
   );
 }
