1842fffb367e9f7e05dbb2c165cd90f8f0cb728d
ChoiBooKi <qnrl9034@naver.com>
2024-11-02 17:37:22 +0900

    [FEAT] Apply Input and modify styles TALEARNT-101

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,39 +1,45 @@
 import { Link, Outlet } from "react-router-dom";
 
+import { classNames } from "@utils/classNames";
+
 import { LogoIcon } from "@components/icons/LogoIcon/LogoIcon";
 import { SearchIcon } from "@components/icons/SearchIcon/SearchIcon";
+import { Input } from "@components/Input/Input";
 
 function MainLayout() {
+  const linkStyle =
+    "rounded-lg flex w-[6.875rem] items-center justify-center text-[1.125rem] font-medium";
+
   return (
     <div>
       <header
         className={
-          "flex h-[90px] gap-[127px] justify-center items-center shadow-[inset_0_-1px_0_0] shadow-talearnt-gray-200"
+          "flex h-[88px] items-center justify-between px-20 shadow-[inset_0_-1px_0_0] shadow-talearnt-Line_01"
         }
       >
         <LogoIcon />
-        <div className={"relative w-[500px] h-[40px]"}>
-          <input
-            className={
-              "w-full border border-talearnt-primary rounded-full focus:outline-none py-[9px] pl-[15px] pr-[55px] text-sm leading-[22px] placeholder:text-gray-500"
-            }
-            placeholder={"관심있는 재능 키워드를 입력해 주세요."}
-          />
-          <SearchIcon className={"absolute top-2 right-4 cursor-pointer"} />
-        </div>
-        <div className={"flex gap-[16px] h-[40px]"}>
+        <Input
+          className={
+            "h-10 w-[31.25rem] rounded-full border-talearnt-Primary_01 pr-[55px]"
+          }
+        >
+          <SearchIcon className={"absolute right-4 top-2 cursor-pointer"} />
+        </Input>
+        <div className={"flex h-[40px] gap-[16px]"}>
           <Link
-            className={
-              "flex justify-center items-center w-[100px] rounded-[12px] bg-white border border-gray-300 text-gray-900 hover:bg-gray-50"
-            }
+            className={classNames(
+              linkStyle,
+              "border border-talearnt-Icon_02 text-talearnt-Text_02"
+            )}
             to={"sign-in"}
           >
             로그인
           </Link>
           <Link
-            className={
-              "flex justify-center items-center w-[120px] rounded-[12px] bg-talearnt-primary text-white hover:bg-button-hover"
-            }
+            className={classNames(
+              linkStyle,
+              "bg-talearnt-Primary_01 text-talearnt-BG_Background hover:bg-talearnt-PrimaryBG_02"
+            )}
             to={"sign-up"}
           >
             회원가입
