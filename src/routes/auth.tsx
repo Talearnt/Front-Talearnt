a3e7a9ce81005a1cbda5a2d595f427f7cc9107ef
ChoiBooKi <qnrl9034@naver.com>
2024-11-04 22:28:32 +0900

    [FEAT] Add signUp routes TALEARNT-151

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,11 +1,16 @@
 import { RouteObject } from "react-router-dom";
 
 import SignIn from "@pages/auth/SignIn/SignIn";
+import SignUp from "@pages/auth/SignUp/SignUp";
 
 const authRouter: RouteObject[] = [
   {
     element: <SignIn />,
     path: "sign-in"
+  },
+  {
+    element: <SignUp />,
+    path: "sign-up"
   }
 ];
 
