ffd2273a8e16d186d5dbe637a86355a09cc1b314
ChoiBooKi <qnrl9034@naver.com>
2024-11-05 22:07:30 +0900

    [FEAT] Setting button action TALEARNT-151

--- src/pages/auth/SignUp/components/StepFormContainer/StepFormContainer.tsx
+++ src/pages/auth/SignUp/components/StepFormContainer/StepFormContainer.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useFormContext } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+import { classNames } from "@utils/classNames";
 
 import { Button } from "@components/Button/Button";
 import { Stepper } from "@components/Stepper/Stepper";
@@ -8,29 +11,69 @@ import { Agreements } from "@pages/auth/SignUp/components/Agreements/Agreements"
 const stepArray = ["약관 동의", "정보 입력", "가입 완료"];
 
 function StepFormContainer() {
-  const { getValues } = useFormContext();
+  const navigator = useNavigate();
+  const { watch } = useFormContext();
 
   const [step, setStep] = useState(1);
+
+  const agreement0 = watch("agreement0");
+  const agreement1 = watch("agreement1");
 
   const handleCTAOnClick = () => setStep(prev => prev + 1);
 
   return (
-    <div className={"mx-auto mt-[6.5rem] flex w-[39.5rem] flex-col"}>
+    <div
+      className={"mx-auto mb-[7.875rem] mt-[6.5rem] flex w-[39.5rem] flex-col"}
+    >
       <Stepper
         className={"mb-[4.125rem]"}
         currentStep={step}
         stepArray={stepArray}
       />
       <p className={"mb-10 text-center text-3xl font-semibold"}>
-        Talearnt 서비스 이용을 위한
-        <br />
-        약관에 동의해 주세요
+        {step === 1 && (
+          <>
+            Talearnt 서비스 이용을 위한
+            <br />
+            약관에 동의해 주세요
+          </>
+        )}
+        {step === 2 && (
+          <>
+            로그인에 사용할
+            <br />
+            회원정보를 입력해 주세요
+          </>
+        )}
+        {step === 3 && (
+          <>
+            성공적으로 가입되었어요
+            <br />
+            Talearnt에서 만나요!
+          </>
+        )}
       </p>
       {step === 1 && <Agreements />}
-      <Button onClick={handleCTAOnClick}>
-        {step === 1 && "시작 하기"}
-        {step === 2 && "가입 하기"}
-      </Button>
+      <div
+        className={classNames(
+          "mt-[3.5rem] grid grid-rows-[3.125rem] gap-[1.975rem]",
+          step === 3 ? "grid-cols-2" : "grid-cols-1"
+        )}
+      >
+        {step === 3 && (
+          <Button buttonStyle={"outlined"} onClick={() => navigator("/")}>
+            홈으로
+          </Button>
+        )}
+        <Button
+          disabled={!agreement0 || !agreement1}
+          onClick={handleCTAOnClick}
+        >
+          {step === 1 && "시작 하기"}
+          {step === 2 && "가입 하기"}
+          {step === 3 && "로그인"}
+        </Button>
+      </div>
     </div>
   );
 }
