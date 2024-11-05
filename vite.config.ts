ba0a8d0adc276a708e276998afcc8d2773a8e304
ChoiBooKi <qnrl9034@naver.com>
2024-11-05 15:57:30 +0900

    [FEAT] Set integration test TALEARNT-149

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { ViteUserConfig } from "vitest/dist/config";
 
-// https://vitejs.dev/config/
+const vitestConfig: ViteUserConfig = {
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts"
+  }
+};
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  ...vitestConfig
 });
