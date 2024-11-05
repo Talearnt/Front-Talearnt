baf0270b806c82496f7daaf7ce7a6a4be44c2dff
ChoiBooKi <qnrl9034@naver.com>
2024-11-05 15:02:52 +0900

    [FEAT] Make describe test group TALEARNT-149

--- src/utils/__tests__/checkObjectType.test.ts
+++ src/utils/__tests__/checkObjectType.test.ts
@@ -1,29 +1,31 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import { checkObjectType } from "@utils/checkObjectType";
 
-test("typeof {} -> object, object to return true", () => {
-  expect(checkObjectType({})).toBe(true);
-});
+describe("checkObjectType utility function", () => {
+  test("should return true for an object", () => {
+    expect(checkObjectType({})).toBe(true);
+  });
 
-test("typeof [] -> object, Array to return false", () => {
-  expect(checkObjectType([])).toBe(false);
-});
+  test("should return false for an array", () => {
+    expect(checkObjectType([])).toBe(false);
+  });
 
-test("typeof null -> null | null to return false", () => {
-  expect(checkObjectType(null)).toBe(false);
-});
+  test("should return false for null", () => {
+    expect(checkObjectType(null)).toBe(false);
+  });
 
-test("other types return false", () => {
-  expect(checkObjectType("")).toBe(false);
-  expect(checkObjectType(1)).toBe(false);
-  expect(checkObjectType(undefined)).toBe(false);
-});
+  test("should return false for non-object types", () => {
+    expect(checkObjectType("")).toBe(false);
+    expect(checkObjectType(1)).toBe(false);
+    expect(checkObjectType(undefined)).toBe(false);
+  });
 
-test("nested object to return true", () => {
-  expect(checkObjectType({ a: { b: 2 } })).toBe(true);
-});
+  test("should return true for a nested object", () => {
+    expect(checkObjectType({ a: { b: 2 } })).toBe(true);
+  });
 
-test("empty object to return true", () => {
-  expect(checkObjectType({})).toBe(true);
+  test("should return true for an empty object", () => {
+    expect(checkObjectType({})).toBe(true);
+  });
 });
