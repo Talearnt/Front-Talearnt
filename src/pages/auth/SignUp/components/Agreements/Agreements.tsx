be993b95b13c98091461f73bb12393059cd12796
ChoiBooKi <qnrl9034@naver.com>
2024-11-05 22:05:33 +0900

    [FEAT] Change Agreements.tsx TALEARNT-151

--- src/pages/auth/SignUp/components/Agreements/Agreements.tsx
+++ src/pages/auth/SignUp/components/Agreements/Agreements.tsx
@@ -3,6 +3,30 @@ import { useFormContext } from "react-hook-form";
 
 import { CheckBox } from "@components/CheckBox/CheckBox";
 
+type agreementsDataType = {
+  required: boolean;
+  title: string;
+}[];
+
+const agreementsData: agreementsDataType = [
+  {
+    required: true,
+    title: "이용약관 동의"
+  },
+  {
+    required: true,
+    title: "개인정보 수집"
+  },
+  {
+    required: false,
+    title: "마케팅 목적의 개인정보 수집 및 이용 동의"
+  },
+  {
+    required: false,
+    title: "광고성 정보 수신 동의"
+  }
+];
+
 function Agreements() {
   const { register, setValue } = useFormContext();
 
@@ -12,10 +36,9 @@ function Agreements() {
     const isChecked = target.checked;
 
     setValue("all", isChecked);
-    setValue("agreement1", isChecked);
-    setValue("agreement2", isChecked);
-    setValue("agreement3", isChecked);
-    setValue("agreement4", isChecked);
+    agreementsData.forEach((_, index) =>
+      setValue(`agreement${index.toString()}`, isChecked)
+    );
   };
 
   return (
@@ -29,39 +52,23 @@ function Agreements() {
           전체 동의하기 (선택 정보를 포함합니다.)
         </p>
       </CheckBox>
-      <CheckBox
-        className={"h-[4.375rem] gap-4 border-b border-b-talearnt-Line_01"}
-        formData={{ ...register("agreement1") }}
-      >
-        <p className={"w-full text-base font-semibold"}>
-          <span className={"text-talearnt-Error_01"}>(필수)</span> 이용약관 동의
-        </p>
-      </CheckBox>
-      <CheckBox
-        className={"h-[4.375rem] gap-4 border-b border-b-talearnt-Line_01"}
-        formData={{ ...register("agreement2") }}
-      >
-        <p className={"w-full text-base font-semibold"}>
-          <span className={"text-talearnt-Error_01"}>(필수)</span> 개인정보 수집
-          및 이용 동의
-        </p>
-      </CheckBox>
-      <CheckBox
-        className={"h-[4.375rem] gap-4 border-b border-b-talearnt-Line_01"}
-        formData={{ ...register("agreement3") }}
-      >
-        <p className={"w-full text-base font-semibold"}>
-          (선택) 마케팅 목적의 개인정보 수집 및 이용 동의
-        </p>
-      </CheckBox>
-      <CheckBox
-        className={"h-[4.375rem] gap-4 border-b border-b-talearnt-Line_01"}
-        formData={{ ...register("agreement4") }}
-      >
-        <p className={"w-full text-base font-semibold"}>
-          (선택) 광고성 정보 수신 동의
-        </p>
-      </CheckBox>
+      {agreementsData.map(({ required, title }, index) => (
+        <CheckBox
+          className={"h-[4.375rem] gap-4 border-b border-b-talearnt-Line_01"}
+          formData={{ ...register(`agreement${index.toString()}`) }}
+          key={`agreement${index.toString()}`}
+        >
+          <p className={"w-full text-base font-semibold"}>
+            {required ? (
+              <span className={"text-talearnt-Error_01"}>(필수)</span>
+            ) : (
+              "(선택)"
+            )}
+            &nbsp;
+            {title}
+          </p>
+        </CheckBox>
+      ))}
     </div>
   );
 }
