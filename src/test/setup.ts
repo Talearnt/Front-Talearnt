b81ef78f793f017a64098a1002724f66ddbbc073
ChoiBooKi <qnrl9034@naver.com>
2024-11-06 16:29:49 +0900

    [FEAT] Install web-streams-polyfill TALEARNT-150

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,6 +2,7 @@ import { server } from "@/test/api/mock/server";
 import { afterAll, afterEach, beforeAll } from "vitest";
 
 import "@testing-library/jest-dom/vitest";
+import "web-streams-polyfill/ponyfill";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
