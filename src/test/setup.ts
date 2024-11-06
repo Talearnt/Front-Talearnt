565d595ef3423696a05eb0bbc5c0924d6a147734
ChoiBooKi <qnrl9034@naver.com>
2024-11-06 16:15:42 +0900

    [FEAT] Setup MSW TALEARNT-150

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,8 @@
+import { server } from "@/test/api/mock/server";
+import { afterAll, afterEach, beforeAll } from "vitest";
+
 import "@testing-library/jest-dom/vitest";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
