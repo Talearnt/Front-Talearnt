7396d7c6d082ae3a13867e99fd2e28768b8de758
ChoiBooKi <qnrl9034@naver.com>
2024-11-06 16:37:25 +0900

    [FEAT] Change web-streams-polyfill TALEARNT-150

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,6 +2,7 @@ import { server } from "@/test/api/mock/server";
 import { afterAll, afterEach, beforeAll } from "vitest";
 
 import "@testing-library/jest-dom/vitest";
+import "web-streams-polyfill";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
