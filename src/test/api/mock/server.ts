59245b630eac4021556287e4ae8021c5d2394d10
ChoiBooKi <qnrl9034@naver.com>
2024-11-06 17:06:34 +0900

    [FEAT] Test

--- src/test/api/mock/server.ts
+++ src/test/api/mock/server.ts
@@ -1,6 +1,5 @@
 import { setupServer } from "msw/node";
 
-import "web-streams-polyfill";
 import { handlers } from "./handler";
 
 export const server = setupServer(...handlers);
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,9 +1,8 @@
 import { server } from "@/test/api/mock/server";
 import { afterAll, afterEach, beforeAll } from "vitest";
-import { TransformStream } from "web-streams-polyfill";
 
+import "web-streams-polyfill/polyfill";
 import "@testing-library/jest-dom/vitest";
-globalThis.TransformStream = TransformStream;
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
