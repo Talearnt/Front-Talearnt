969adb2426dd3e857282afa210c4ab6ad29dc94e
ChoiBooKi <qnrl9034@naver.com>
2024-11-06 17:16:09 +0900

    Test

--- src/test/api/mock/server.ts
+++ src/test/api/mock/server.ts
@@ -1,3 +1,10 @@
+import "web-streams-polyfill/polyfill";
+import "broadcast-channel";
+
+if (typeof globalThis.BroadcastChannel === "undefined") {
+  globalThis.BroadcastChannel = BroadcastChannel;
+}
+
 import { setupServer } from "msw/node";
 
 import { handlers } from "./handler";
