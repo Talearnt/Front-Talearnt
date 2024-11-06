a90b1a9dacf55a76d36c56974952351f396fcc1f
ChoiBooKi <qnrl9034@naver.com>
2024-11-06 17:12:58 +0900

    Test

--- src/test/api/mock/server.ts
+++ src/test/api/mock/server.ts
@@ -1,7 +1,3 @@
-/* eslint-disable simple-import-sort/imports */
-import "web-streams-polyfill/polyfill";
-import "broadcast-channel";
-
 import { setupServer } from "msw/node";
 
 import { handlers } from "./handler";
