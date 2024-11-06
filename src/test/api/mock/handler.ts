e6dbb6a1da2c3c3bf3de0006a290566bd335e826
ChoiBooKi <qnrl9034@naver.com>
2024-11-06 16:15:49 +0900

    [FEAT] Make MSW handler TALEARNT-150

---
+++ src/test/api/mock/handler.ts
@@ -0,0 +1,39 @@
+import { http, HttpResponse } from "msw";
+import { PathParams } from "msw/src/core/utils/matching/matchRequestUrl";
+
+import { accountType } from "@pages/auth/api/auth.type";
+
+export const handlers = [
+  http.post<PathParams, accountType>(
+    "http://3.35.198.221/v1/api/auth/login",
+    async ({ request }) => {
+      const { userId, pw } = await request.json();
+
+      // 유효한 자격 증명
+      if (userId === "test@test.com" && pw === "test") {
+        return HttpResponse.json(
+          {
+            data: {
+              accessToken: "valid_token"
+            },
+            errorCode: null,
+            errorMessage: null,
+            success: true
+          },
+          { status: 200 }
+        );
+      }
+
+      // 무효한 자격 증명
+      return HttpResponse.json(
+        {
+          data: null,
+          errorCode: "USER_NOT_FOUND",
+          errorMessage: "Invalid credentials",
+          success: false
+        },
+        { status: 404 }
+      );
+    }
+  )
+];
