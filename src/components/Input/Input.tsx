b90ac1c2e3788dad0fb5fc3da0964eb95f47c6ae
ChoiBooKi <qnrl9034@naver.com>
2024-11-07 23:03:00 +0900

    [FEAT] Change Input.tsx TALEARNT-151

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -47,34 +47,34 @@ function Input({
   ...props
 }: InputProps) {
   return (
-    <div
-      className={classNames("relative flex w-full flex-col", wrapperClassName)}
-    >
-      {label && (
-        <label className={"mb-2 text-base"} htmlFor={id}>
-          {label}
-        </label>
-      )}
-      <input
-        className={classNames(
-          "h-[50px] w-full rounded-lg border bg-talearnt-BG_Background px-[15px] text-[1rem] placeholder:text-talearnt-Text_04 focus:border-talearnt-Primary_01 focus:outline-none disabled:cursor-not-allowed disabled:bg-talearnt-BG_Up_01",
-          error?.hasError
-            ? "border-talearnt-Error_02 focus:border-talearnt-Error_02"
-            : "border-talearnt-Line_01",
-          className
+    <div className={classNames(wrapperClassName)}>
+      <div className={"flex w-full flex-col"}>
+        {label && (
+          <label className={"mb-2 text-base"} htmlFor={id}>
+            {label}
+          </label>
         )}
-        id={id}
-        {...formData}
-        {...props}
-      />
-      {error?.hasError && error.errorContent && (
-        <div className={"mt-1 flex items-center gap-0.5"}>
-          <ErrorIcon />
-          <p className={"text-[0.875rem] font-medium text-talearnt-Error_01"}>
-            {error.errorContent}
-          </p>
-        </div>
-      )}
+        <input
+          className={classNames(
+            "h-[50px] w-full rounded-lg border bg-talearnt-BG_Background px-[15px] text-[1rem] placeholder:text-talearnt-Text_04 focus:border-talearnt-Primary_01 focus:outline-none disabled:cursor-not-allowed disabled:bg-talearnt-BG_Up_01",
+            error?.hasError
+              ? "border-talearnt-Error_02 focus:border-talearnt-Error_02"
+              : "border-talearnt-Line_01",
+            className
+          )}
+          id={id}
+          {...formData}
+          {...props}
+        />
+        {error?.hasError && error.errorContent && (
+          <div className={"mt-1 flex items-center gap-0.5"}>
+            <ErrorIcon />
+            <p className={"text-[0.875rem] font-medium text-talearnt-Error_01"}>
+              {error.errorContent}
+            </p>
+          </div>
+        )}
+      </div>
       {children}
     </div>
   );
