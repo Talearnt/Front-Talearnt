611a7fa62c1e6ec1adf3a4136e51e18218ebb72a
ChoiBooKi <qnrl9034@naver.com>
2024-11-08 11:11:56 +0900

    [FEAT] Test TALEARNT-150

--- src/test/polyfill.ts
+++ src/test/polyfill.ts
@@ -1,5 +1,12 @@
-import { BroadcastChannel } from "broadcast-channel";
+// polyfill.ts
+import { BroadcastChannel as PolyfillBroadcastChannel } from "broadcast-channel";
 
-if (typeof global.BroadcastChannel === "undefined") {
-  global.BroadcastChannel = BroadcastChannel;
+import "web-streams-polyfill/polyfill";
+
+// Node.js 환경에서만 BroadcastChannel을 폴리필합니다.
+if (
+  typeof window === "undefined" &&
+  typeof global.BroadcastChannel === "undefined"
+) {
+  global.BroadcastChannel = PolyfillBroadcastChannel;
 }
