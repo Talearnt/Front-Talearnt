001323dafdd02d8d0a14517c343f0cd4cdab22c2
ChoiBooKi <qnrl9034@naver.com>
2024-11-08 11:05:22 +0900

    [FEAT] Test TALEARNT-150

--- src/test/api/mock/handler.ts
+++ src/test/api/mock/handler.ts
@@ -1,5 +1,5 @@
 /* eslint-disable simple-import-sort/imports */
-import "broadcast-channel";
+import { BroadcastChannel } from "broadcast-channel";
 
 import { http, HttpResponse } from "msw";
 import { PathParams } from "msw/src/core/utils/matching/matchRequestUrl";
