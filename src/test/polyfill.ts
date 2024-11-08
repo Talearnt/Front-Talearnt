08cd2ed119c146e030966898325c3b85f1264cee
ChoiBooKi <qnrl9034@naver.com>
2024-11-08 11:24:08 +0900

    [FEAT] Test TALEARNT-150

--- src/test/polyfill.ts
+++ src/test/polyfill.ts
@@ -1,2 +1,15 @@
+import { Blob, File } from "node:buffer";
+import { fetch, FormData, Headers, Request, Response } from "undici";
+
 import "broadcastchannel-polyfill/index.js";
 import "web-streams-polyfill/polyfill";
+
+Object.defineProperties(globalThis, {
+  fetch: { value: fetch, writable: true },
+  Blob: { value: Blob },
+  File: { value: File },
+  Headers: { value: Headers },
+  FormData: { value: FormData },
+  Request: { value: Request },
+  Response: { value: Response }
+});
