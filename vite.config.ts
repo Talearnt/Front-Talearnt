846ebcbe2a2ff6dde8e0d4cd2864649f2769cf51
ChoiBooKi <qnrl9034@naver.com>
2024-11-08 10:55:24 +0900

    [FEAT] Test TALEARNT-150

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const vitestConfig: ViteUserConfig = {
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts", "./src/test/polyfills.ts"],
+    setupFiles: "./src/test/setup.ts",
     reporters: ["junit", "default"],
     outputFile: "test-results.xml"
   }
