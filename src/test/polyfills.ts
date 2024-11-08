ffc9158d9850966c0c2bc4b57568c147e145a64a
ChoiBooKi <qnrl9034@naver.com>
2024-11-08 10:41:43 +0900

    [FEAT] Add polyfills.ts TALEARNT-150

--- src/test/polyfills.ts
+++ src/test/polyfills.ts
@@ -1,5 +1,5 @@
-import { TransformStream } from "web-streams-polyfill";
-
 if (typeof global.TransformStream === "undefined") {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const { TransformStream } = require("web-streams-polyfill");
   global.TransformStream = TransformStream;
 }
