eac43b26591b003697e3d73ecbf097b7bbd0f3ae
ChoiBooKi <qnrl9034@naver.com>
2024-11-22 11:19:49 +0900

    [FEAT] Modify FindPassword.tsx TALEARNT-105

--- src/pages/auth/FindAccount/components/FindPassword/FindPassword.tsx
+++ src/pages/auth/FindAccount/components/FindPassword/FindPassword.tsx
@@ -5,18 +5,9 @@ import { Input } from "@components/Input/Input";
 
 function FindPassword() {
   return (
-    <div className={classNames("flex flex-col gap-6", "mb-10")}>
-      <Input label={"휴대폰 본인인증"}>
-        <Button buttonStyle={"outlined"} className={"ml-2 w-[150px] shrink-0"}>
-          인증번호 요청
-        </Button>
-      </Input>
-      <Input label={"인증번호 확인"}>
-        <Button buttonStyle={"outlined"} className={"ml-2 w-[95px] shrink-0"}>
-          확인
-        </Button>
-      </Input>
-      <Button className={"mt-4"}>아이디 찾기</Button>
+    <div className={classNames("flex flex-col gap-[40px]")}>
+      <Input label={"이메일"} />
+      <Button>비밀번호 찾기</Button>
     </div>
   );
 }
