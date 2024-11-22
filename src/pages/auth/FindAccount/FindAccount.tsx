1f8a2ca2cc23eb5703a66436ec7526b235ec27e2
ChoiBooKi <qnrl9034@naver.com>
2024-11-22 11:17:15 +0900

    [FEAT] Move top content to FindAccount.tsx TALEARNT-105

--- src/pages/auth/FindAccount/FindAccount.tsx
+++ src/pages/auth/FindAccount/FindAccount.tsx
@@ -20,15 +20,32 @@ function FindAccount() {
   const { pathname } = useLocation();
 
   const pathArray = pathname.split("/");
+  const currentPage = pathArray[pathArray.length - 1];
 
   return (
     <div className={classNames("flex flex-col gap-[56px]", "w-[632px]")}>
       <TabSlider
         className={"text-lg"}
-        currentValue={pathArray[pathArray.length - 1]}
+        currentValue={currentPage}
         onClickHandler={value => navigator(`/find-account/${value}`)}
         options={options}
       />
+      <p className={classNames("text-center text-3xl font-semibold")}>
+        {currentPage === "id" && (
+          <>
+            가입 시 등록한 휴대폰 정보로
+            <br />
+            아이디를 찾을 수 있어요
+          </>
+        )}
+        {currentPage === "pw" && (
+          <>
+            본인 확인을 통해
+            <br />
+            비밀번호를 재설정할 수 있어요
+          </>
+        )}
+      </p>
       <Outlet />
     </div>
   );
