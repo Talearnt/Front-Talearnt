c32a5f25c15f93782b977f4db46ea8fe060d3a32
ChoiBooKi <qnrl9034@naver.com>
2024-12-02 22:13:52 +0900

    [FEAT] Make sms api TALEARNT-197

--- src/pages/auth/api/auth.api.ts
+++ src/pages/auth/api/auth.api.ts
@@ -1,7 +1,11 @@
 import { postAPI } from "@utils/apiMethods";
 
 import { customAxiosResponseType } from "@common/common.type";
-import { accountType } from "@pages/auth/api/auth.type";
+import {
+  accountType,
+  submitVerificationBodyType,
+  verificationBodyType
+} from "@pages/auth/api/auth.type";
 
 export const postSignIn = async (
   account: accountType
@@ -10,3 +14,10 @@ export const postSignIn = async (
 
 export const postToGetRefreshToken = async () =>
   await postAPI("/v1/api/auth/refresh");
+
+export const postSendVerificationCode = async (data: verificationBodyType) =>
+  await postAPI("/v1/auth/sms/verification-codes", data);
+
+export const postSubmitVerificationCode = async (
+  data: submitVerificationBodyType
+) => await postAPI("/v1/auth/sms/validation", data);
