e7721519434076d71aa12ddd1e8de2e460164661
ChoiBooKi <qnrl9034@naver.com>
2024-12-02 22:13:52 +0900

    [FEAT] Make sms api TALEARNT-197

--- src/pages/auth/api/auth.type.ts
+++ src/pages/auth/api/auth.type.ts
@@ -2,3 +2,12 @@ export type accountType = {
   userId: string;
   pw: string;
 };
+
+export type verificationBodyType = {
+  type?: "회원가입" | "아이디찾기";
+  phone: string;
+};
+
+export type submitVerificationBodyType = verificationBodyType & {
+  code: string;
+};
