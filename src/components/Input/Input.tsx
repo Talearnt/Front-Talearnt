dece58dd0f745d09b08b8ce1af1c2a0037cb47d3
ChoiBooKi <qnrl9034@naver.com>
2024-12-04 22:49:38 +0900

    [FEAT] Add complete helper text TALEARNT-236

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,14 +3,13 @@ import { UseFormRegisterReturn } from "react-hook-form/dist/types/form";
 
 import { classNames } from "@utils/classNames";
 
+import { CompleteIcon } from "@components/icons/CompleteIcon/CompleteIcon";
 import { ErrorIcon } from "@components/icons/ErrorIcon/ErrorIcon";
 
 type InputProps = ComponentProps<"input"> & {
   children?: ReactNode;
-  error?: {
-    errorContent: string;
-    hasError: boolean;
-  };
+  complete?: string;
+  error?: string;
   formData?: UseFormRegisterReturn;
   label?: string;
   wrapperClassName?: string;
@@ -20,15 +19,15 @@ type InputProps = ComponentProps<"input"> & {
  *
  * Input 컴포넌트
  * children - Node
- * error.errorContent - 에러인 경우 보여줄 내용
- * error.hasError - 에러인지 알려주는 플래그
+ * error - 에러인 경우 보여줄 내용
  * label - label에 들어갈 내용
  * wrapperClassName - input을 감싸고 있는 wrapper에 들어갈 className
  * formData - react-hook-form 에서 사용하는 값들
  *
  * @param {string | undefined} className
+ * @param {string | undefined} complete
  * @param {React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | undefined | null | (undefined & React.ReactElement<any, string | React.JSXElementConstructor<any>>) | (undefined & Iterable<React.ReactNode>) | (undefined & React.ReactPortal)} children
- * @param {{errorContent: string, hasError: boolean} | undefined} error
+ * @param {string | undefined} error
  * @param {UseFormRegisterReturn<UseFormRegisterReturn> | undefined} formData
  * @param {string | undefined} id
  * @param {string | undefined} label
@@ -38,6 +37,7 @@ type InputProps = ComponentProps<"input"> & {
  */
 function Input({
   className,
+  complete,
   children,
   error,
   formData,
@@ -46,6 +46,8 @@ function Input({
   wrapperClassName,
   ...props
 }: InputProps) {
+  const hasComplete = complete !== undefined;
+  const hasError = error !== undefined;
   return (
     <div className={classNames("flex flex-col", "w-full", wrapperClassName)}>
       {label && (
@@ -53,11 +55,15 @@ function Input({
           {label}
         </label>
       )}
-      <div className={classNames("flex")}>
+      <div className={classNames("relative flex")}>
         <input
           className={classNames(
-            "h-[50px] w-full rounded-lg border bg-talearnt-BG_Background px-[15px] text-[1rem] placeholder:text-talearnt-Text_04 focus:border-talearnt-Primary_01 focus:outline-none disabled:cursor-not-allowed disabled:bg-talearnt-BG_Up_01",
-            error?.hasError
+            "rounded-lg border bg-talearnt-BG_Background px-[15px] text-[1rem]",
+            "h-[50px] w-full",
+            "placeholder:text-talearnt-Text_04",
+            "focus:border-talearnt-Primary_01 focus:outline-none",
+            "disabled:cursor-not-allowed disabled:bg-talearnt-BG_Up_01 disabled:text-talearnt-Text_04",
+            hasError
               ? "border-talearnt-Error_02 focus:border-talearnt-Error_02"
               : "border-talearnt-Line_01",
             className
@@ -68,11 +74,19 @@ function Input({
         />
         {children}
       </div>
-      {error?.hasError && error.errorContent && (
+      {(hasComplete || hasError) && (
         <div className={"mt-1 flex items-center gap-0.5"}>
-          <ErrorIcon />
-          <p className={"text-[0.875rem] font-medium text-talearnt-Error_01"}>
-            {error.errorContent}
+          {hasComplete && <CompleteIcon />}
+          {hasError && <ErrorIcon />}
+          <p
+            className={classNames(
+              "text-[0.875rem] font-medium",
+              hasError ? "text-talearnt-Error_01" : "",
+              hasComplete ? "text-talearnt-Primary_01" : ""
+            )}
+          >
+            {hasComplete && complete}
+            {hasError && error}
           </p>
         </div>
       )}
