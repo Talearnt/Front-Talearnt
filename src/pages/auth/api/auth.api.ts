7f2e185893900c095aea3701d999f6103ab42937
ChoiBooKi <qnrl9034@naver.com>
2024-12-08 18:28:41 +0900

    [FEAT] Make checkUserId api TALEARNT-236

--- src/pages/auth/api/auth.api.ts
+++ src/pages/auth/api/auth.api.ts
@@ -7,20 +7,29 @@ import {
   verificationBodyType
 } from "@pages/auth/api/auth.type";
 
+// 로그인
 export const postSignIn = async (
   account: accountType
 ): Promise<customAxiosResponseType<{ accessToken: string }>> =>
   await postAPI("/v1/api/auth/login", account);
 
+// refresh 토큰
 export const postToGetRefreshToken = async () =>
   await postAPI("/v1/api/auth/refresh");
 
+// 인증번호 전송
 export const postSendVerificationCode = async (data: verificationBodyType) =>
   await postAPI("/v1/auth/sms/verification-codes", data);
 
+// 인증번호 검증
 export const postConfirmVerificationCode = async (
   data: submitVerificationBodyType
 ) => await postAPI("/v1/auth/sms/validation", data);
 
+// 랜덤 닉네임 생성, 닉네임 중복 확인
 export const getNickNameWithCheck = async (nickName?: string) =>
-  await getAPI<string>("/v1/auth/users/nickname", { nickName });
+  await getAPI<string | boolean>("/v1/auth/users/nickname", { nickName });
+
+// 인증번호 검증
+export const getCheckNickName = async (userId: string) =>
+  await getAPI<boolean>("/v1/auth/users/id", { userId });
