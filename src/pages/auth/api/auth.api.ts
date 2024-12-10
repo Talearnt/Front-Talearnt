1e5701651af0a7644732f84883defa70f180e369
ChoiBooKi <qnrl9034@naver.com>
2024-12-10 18:20:44 +0900

    [FEAT] 닉네임 중복 확인 API 추가 | Add check nickname api TALEARNT-236

--- src/pages/auth/api/auth.api.ts
+++ src/pages/auth/api/auth.api.ts
@@ -26,10 +26,14 @@ export const postConfirmVerificationCode = async (
   data: submitVerificationBodyType
 ) => await postAPI("/v1/auth/sms/validation", data);
 
-// 랜덤 닉네임 생성, 닉네임 중복 확인
-export const getNickNameWithCheck = async (nickName?: string) =>
-  await getAPI<string | boolean>("/v1/auth/users/nickname", { nickName });
+// 랜덤 닉네임 생성
+export const getRandomNickName = async () =>
+  await getAPI<string>("/v1/auth/users/nickname/random");
 
-// 인증번호 검증
-export const getCheckNickName = async (userId: string) =>
+// 닉네임 중복 확인
+export const getCheckNickName = async (nickname: string) =>
+  await getAPI<boolean>("/v1/auth/users/nickname/availability", { nickname });
+
+// 아이디 중복 확인
+export const getCheckUserId = async (userId: string) =>
   await getAPI<boolean>("/v1/auth/users/id", { userId });
