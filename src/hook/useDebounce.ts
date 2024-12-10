efb2468654029a0c391e05d7929fa65a3a328520
ChoiBooKi <qnrl9034@naver.com>
2024-12-10 18:19:52 +0900

    [FEAT] useDebounce 수정 | Modify useDebounce TALEARNT-236

--- src/hook/useDebounce.ts
+++ src/hook/useDebounce.ts
@@ -4,6 +4,11 @@ const useDebounce = <T = string>(value: T, delay = 300) => {
   const [debouncedValue, setDebouncedValue] = useState<T | undefined>(value);
 
   useEffect(() => {
+    if (!value) {
+      // falsy 한 값이면 return
+      return;
+    }
+
     // 타이머가 시작됩니다.
     const handler = setTimeout(() => {
       setDebouncedValue(value); // 지정된 delay 후에 값을 업데이트
@@ -17,7 +22,7 @@ const useDebounce = <T = string>(value: T, delay = 300) => {
     };
   }, [value, delay]); // value나 delay가 변경될 때마다 실행됩니다.
 
-  return debouncedValue; // 디바운싱된 값을 반환합니다.
+  return !value ? value : debouncedValue; // 디바운싱된 값을 반환합니다.
 };
 
 export default useDebounce;
