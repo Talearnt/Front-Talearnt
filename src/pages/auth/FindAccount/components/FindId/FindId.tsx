64b4497f49d57169ec91d13606870380cc19efe9
ChoiBooKi <qnrl9034@naver.com>
2024-12-13 23:06:02 +0900

    [FEAT] 아이디 찾기 완료 마크업 | Complete find id markup TALEARNT-197

--- src/pages/auth/FindAccount/components/FindId/FindId.tsx
+++ src/pages/auth/FindAccount/components/FindId/FindId.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 import { yupResolver } from "@hookform/resolvers/yup";
+import dayjs from "dayjs";
 import { object, string } from "yup";
 
 import {
@@ -13,10 +15,14 @@ import { checkObjectType } from "@utils/checkObjectType";
 import { classNames } from "@utils/classNames";
 
 import { Button } from "@components/Button/Button";
+import { CircleCheckIcon } from "@components/icons/CircleCheckIcon/CircleCheckIcon";
 import { Input } from "@components/Input/Input";
 import { VerificationCode } from "@pages/auth/components/VerificationCode/VerificationCode";
 
-import { verificationStateType } from "@pages/auth/api/auth.type";
+import {
+  findIdResponseType,
+  verificationStateType
+} from "@pages/auth/api/auth.type";
 
 const findIdSchema = object({
   name: string().matches(
@@ -26,6 +32,12 @@ const findIdSchema = object({
 }).required();
 
 function FindId() {
+  const navigator = useNavigate();
+  const idData = useRef<{ userId: string; createdAt: string }>({
+    userId: "",
+    createdAt: ""
+  });
+
   const {
     formState: { errors },
     register,
@@ -35,6 +47,7 @@ function FindId() {
     resolver: yupResolver(findIdSchema)
   });
 
+  const [canProceed, setCanProceed] = useState(false);
   const [verification, setVerification] = useState<verificationStateType>({
     isCodeVerified: false
   });
@@ -55,7 +68,7 @@ function FindId() {
         code: verificationCode
       });
 
-      console.log(data);
+      idData.current = data as findIdResponseType;
 
       return true;
     } catch (e) {
@@ -79,26 +92,110 @@ function FindId() {
 
   return (
     <>
-      <p className={classNames("text-center text-3xl font-semibold")}>
-        가입 시 등록한 휴대폰 정보로
-        <br />
-        아이디를 찾을 수 있어요
-      </p>
-      <div className={classNames("flex flex-col gap-6")}>
-        <Input
-          error={errors.name?.message}
-          formData={{ ...register("name") }}
-          label={"이름"}
-          placeholder={"이름을 입력해 주세요"}
-        />
-        <VerificationCode
-          confirmCodeHandler={handleConfirmCode}
-          isSendButtonDisabled={!!errors.name}
-          sendCodeHandler={handleSendCode}
-          verificationState={[verification, setVerification]}
-        />
-      </div>
-      <Button disabled={!verification.isCodeVerified}>아이디 찾기</Button>
+      {canProceed ? (
+        <>
+          <div className={"flex flex-col items-center gap-8"}>
+            <h1 className={classNames("text-center text-3xl font-semibold")}>
+              아이디 찾기 완료
+            </h1>
+            <CircleCheckIcon
+              className={"stroke-talearnt-Primary_01"}
+              size={70}
+            />
+            <div
+              className={classNames(
+                "flex flex-col items-center gap-6",
+                "w-full"
+              )}
+            >
+              <span className={"text-base font-medium"}>
+                아이디 찾기 결과 고객님의 아이디 정보는 아래와 같아요
+              </span>
+              <div
+                className={classNames(
+                  "flex flex-col items-center justify-center gap-2",
+                  "w-full rounded-xl border border-talearnt-Line_01 bg-talearnt-BG_Up_01 py-[31px]"
+                )}
+              >
+                <span className={"text-base font-medium"}>
+                  고객님 아이디는
+                  <b className={"font-semibold"}>
+                    {idData.current.userId.split("@")[0].slice(0, -3)}***@
+                    {idData.current.userId.split("@")[1]}
+                  </b>
+                  입니다.
+                </span>
+                <span className={"text-sm font-medium text-talearnt-Error_01"}>
+                  {dayjs(idData.current.createdAt).format("YYYY-MM-DD HH:mm")}에
+                  가입함
+                </span>
+              </div>
+              <div
+                className={classNames(
+                  "flex items-center justify-between",
+                  "w-full"
+                )}
+              >
+                <span className={classNames("ml-4", "text-lg font-medium")}>
+                  010-****-{verification.phone?.slice(-4)}
+                </span>
+                <span
+                  className={classNames(
+                    "flex items-center justify-center",
+                    "h-[50px] w-[160px] rounded-lg border border-talearnt-Icon_02",
+                    "text-lg font-medium text-talearnt-Text_04"
+                  )}
+                >
+                  아이디 발송됨
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className={"flex gap-[30px]"}>
+            <Button
+              buttonStyle={"outlined"}
+              className={"h-[50px] w-full"}
+              onClick={() => navigator("/find-account/pw")}
+            >
+              비밀번호 찾기
+            </Button>
+            <Button
+              className={"h-[50px] w-full"}
+              onClick={() => navigator("/sign-in")}
+            >
+              로그인
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className={classNames("text-center text-3xl font-semibold")}>
+            가입 시 등록한 휴대폰 정보로
+            <br />
+            아이디를 찾을 수 있어요
+          </h1>
+          <div className={classNames("flex flex-col gap-6")}>
+            <Input
+              error={errors.name?.message}
+              formData={{ ...register("name") }}
+              label={"이름"}
+              placeholder={"이름을 입력해 주세요"}
+            />
+            <VerificationCode
+              confirmCodeHandler={handleConfirmCode}
+              isSendButtonDisabled={!!errors.name || !name}
+              sendCodeHandler={handleSendCode}
+              verificationState={[verification, setVerification]}
+            />
+          </div>
+          <Button
+            disabled={!verification.isCodeVerified}
+            onClick={() => setCanProceed(true)}
+          >
+            아이디 찾기
+          </Button>
+        </>
+      )}
     </>
   );
 }
