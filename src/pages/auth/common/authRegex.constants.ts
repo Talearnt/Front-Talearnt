5e9ae6ffb6a763adf4a0345e066af3f47be4e153
ChoiBooKi <qnrl9034@naver.com>
2024-12-18 23:47:09 +0900

    [FEAT] Change InfoFields.tsx duplicate logic

--- src/pages/auth/common/authRegex.constants.ts
+++ src/pages/auth/common/authRegex.constants.ts
@@ -5,3 +5,5 @@ export const pwRegex =
   /^$|^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?~-]).{8,}$/;
 
 export const nameRegex = /^$|^[가-힣]{2,5}$/;
+
+export const nicknameRegex = /^$|^[가-힣a-zA-Z0-9#]{2,12}$/;
