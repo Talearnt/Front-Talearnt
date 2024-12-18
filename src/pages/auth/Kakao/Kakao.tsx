8bf6dc84351d6c2fc9f00eccae798d269f46e056
ChoiBooKi <qnrl9034@naver.com>
2024-12-19 00:46:15 +0900

    [FEAT] Make Kakao wrapper TALEARNT-103

---
+++ src/pages/auth/Kakao/Kakao.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import { classNames } from "@utils/classNames";
+
+import { useKakaoAuthResponseStore } from "@pages/auth/api/auth.store";
+
+function Kakao() {
+  const navigator = useNavigate();
+  const { pathname } = useLocation();
+
+  const { kakaoAuthResponse } = useKakaoAuthResponseStore();
+
+  const pathArray = pathname.split("/");
+  const currentPage = pathArray[pathArray.length - 1];
+
+  useEffect(() => {
+    if (currentPage !== "oauth" && kakaoAuthResponse === null) {
+      navigator("/sign-in");
+    }
+  }, [currentPage, kakaoAuthResponse, navigator]);
+
+  return (
+    <div className={classNames("flex flex-col gap-[56px]", "w-[632px]")}>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Kakao;
