a60b007c3c31e237bf810e969fe80bbc7c4ab8e6
ChoiBooKi <qnrl9034@naver.com>
2024-12-20 22:59:43 +0900

    [FEAT] Install tanstack query TALEARNT-103

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,20 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import router from "@routes/index";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "./index.css";
 
+const mode = import.meta.env.MODE;
+const queryClient = new QueryClient();
 const root = document.getElementById("root");
 
 if (root) {
-  createRoot(root).render(<RouterProvider router={router} />);
+  createRoot(root).render(
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {mode === "development" && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 }
