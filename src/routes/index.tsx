c4497db40733b95d581024d3e47b4da3bc370bc1
ChoiBooKi <qnrl9034@naver.com>
2024-12-20 19:07:50 +0900

    [FEAT] Change refresh token logic TALEARNT-262

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,34 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 
 import authRouter from "@routes/auth";
 
-import { getAccessTokenUseRefreshToken } from "@pages/auth/api/auth.api";
-
-import { useAuthStore } from "@pages/auth/api/auth.store";
-
 import MainLayout from "@layout/MainLayout/MainLayout";
-
-import { accessTokenType } from "@pages/auth/api/auth.type";
-
-const authLoader = async () => {
-  const { accessToken, setAccessToken } = useAuthStore.getState();
-
-  if (!accessToken) {
-    try {
-      const { data } = await getAccessTokenUseRefreshToken();
-      setAccessToken((data as accessTokenType).accessToken);
-    } catch (error) {
-      console.error("Token refresh failed", error); // 에러는 로깅
-    }
-  }
-
-  return null;
-};
 
 const router = createBrowserRouter([
   {
     element: <MainLayout />,
     path: "/",
-    loader: authLoader,
     children: [...authRouter]
   }
 ]);
