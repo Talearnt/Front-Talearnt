8388d189e844611ec53e8864873020de1e3fb64f
ChoiBooKi <qnrl9034@naver.com>
2024-12-22 20:51:26 +0900

    [FEAT] Prompt 개발 TALEARNT-275

--- src/common/common.store.ts
+++ src/common/common.store.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 import { create } from "zustand/react";
 
 type toastType = {
@@ -40,4 +42,21 @@ export const useToastStore = create<toastStoreType>(set => ({
         toastList: [{ ...toast, id, isRemoving: false }, ...toastList]
       };
     })
+}));
+
+type promptType = {
+  title: ReactNode;
+  content: ReactNode;
+  confirmOnClickHandler: () => void;
+  cancelOnClickHandler?: () => void;
+};
+
+type promptStoreType = {
+  promptData?: promptType;
+  setPrompt: (prompt?: promptType) => void;
+};
+
+export const usePromptStore = create<promptStoreType>(set => ({
+  promptData: undefined,
+  setPrompt: prompt => set({ promptData: prompt })
 }));
