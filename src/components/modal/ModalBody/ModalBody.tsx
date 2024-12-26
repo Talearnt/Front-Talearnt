06ed422f9796fd823e6c05ba1131cd317c26e583
ChoiBooKi <qnrl9034@naver.com>
2024-12-26 21:50:59 +0900

    [FEAT] className 추가 | Add props className TALEARNT-304

--- src/components/modal/ModalBody/ModalBody.tsx
+++ src/components/modal/ModalBody/ModalBody.tsx
@@ -3,11 +3,14 @@ import { ReactNode } from "react";
 import { classNames } from "@utils/classNames";
 
 type ModalBodyProps = {
+  className?: string;
   children: ReactNode;
 };
 
-function ModalBody({ children }: ModalBodyProps) {
-  return <div className={classNames("flex flex-col")}>{children}</div>;
+function ModalBody({ className, children }: ModalBodyProps) {
+  return (
+    <div className={classNames("flex flex-col", className)}>{children}</div>
+  );
 }
 
 export { ModalBody };
