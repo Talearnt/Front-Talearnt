ab8c427933d93daa3fa303390ecc1603c4b80de2
ChoiBooKi <qnrl9034@naver.com>
2024-12-27 22:22:14 +0900

    [FEAT] MultiSelectDropdown.tsx 개발 | Make MultiSelectDropdown.tsx TALEARNT-306

---
+++ src/components/MultiSelectDropdown/MultiSelectDropdown.tsx
@@ -0,0 +1,88 @@
+import { Dispatch, SetStateAction } from "react";
+
+import { classNames } from "@utils/classNames";
+
+import { CheckBox } from "@components/CheckBox/CheckBox";
+import { CaretIcon } from "@components/icons/CaretIcon/CaretIcon";
+
+type MultiSelectDropdownProps<T> = {
+  title: string;
+  options: { label: string; value: T }[];
+  onSelectHandler: Dispatch<SetStateAction<T[]>>;
+  selectedValueArray: T[];
+};
+
+function MultiSelectDropdown<T = string>({
+  title,
+  options,
+  onSelectHandler,
+  selectedValueArray
+}: MultiSelectDropdownProps<T>) {
+  return (
+    <div className={classNames("flex flex-col")}>
+      <label
+        className={classNames(
+          "peer/label group/label",
+          "flex items-center justify-between",
+          "m-2 h-[70px] rounded-lg pl-4 pr-2",
+          "cursor-pointer",
+          "hover:bg-talearnt-BG_Up_01 has-[:checked]:bg-talearnt-BG_Up_01"
+        )}
+      >
+        <input className={"peer/checkbox hidden"} type={"checkbox"} />
+        <span
+          className={classNames(
+            "text-lg text-talearnt-Text_04",
+            "group-hover/label:font-medium group-hover/label:text-talearnt-Text_02",
+            "peer-checked/checkbox:text-talearnt-Text_01 peer-checked/checkbox:font-semibold"
+          )}
+        >
+          {title}
+        </span>
+        <CaretIcon className={"peer-checked/checkbox:rotate-180"} />
+      </label>
+      <div
+        className={classNames(
+          "hidden flex-col gap-2 peer-has-[:checked]/label:flex",
+          "px-2 py-1"
+        )}
+      >
+        {options.map(({ label, value }) => {
+          const checked = selectedValueArray.includes(value);
+
+          return (
+            <CheckBox
+              className={classNames(
+                "group/checkbox",
+                "rounded-lg px-4 py-[13px]",
+                "hover:bg-talearnt-BG_Up_01",
+                checked && "bg-talearnt-BG_Up_01"
+              )}
+              checked={checked}
+              onChange={({ target }) => {
+                if (target.checked) {
+                  onSelectHandler(prev => [...prev, value]);
+                } else {
+                  onSelectHandler(prev => prev.filter(item => item !== value));
+                }
+              }}
+              key={`${label}-${String(value)}`}
+            >
+              <span
+                className={classNames(
+                  "text-lg text-talearnt-Text_04",
+                  "group-hover/checkbox:font-medium group-hover/checkbox:text-talearnt-Text_02",
+                  checked && "!text-talearnt-Text_01 !font-semibold"
+                )}
+              >
+                {label}
+              </span>
+            </CheckBox>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export { MultiSelectDropdown };
