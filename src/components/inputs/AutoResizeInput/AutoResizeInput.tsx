72d7181920fee80ea09e2bb2b8658b0913768bb3
ChoiBooKi <qnrl9034@naver.com>
2025-01-17 14:54:14 +0900

    [FEAT] AutoResizeInput.tsx 개발 | Make AutoResizeInput.tsx TALEARNT-332

---
+++ src/components/inputs/AutoResizeInput/AutoResizeInput.tsx
@@ -0,0 +1,41 @@
+import { ComponentProps, useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+
+function AutoResizeInput({ onChange, ...props }: ComponentProps<"input">) {
+  const measureWidthRef = useRef<HTMLSpanElement>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const { register, watch } = useForm<{ text: string }>();
+
+  const value = watch("text");
+
+  const combinedRef = (node: HTMLInputElement | null) => {
+    inputRef.current = node;
+    register("text").ref(node);
+  };
+
+  useEffect(() => {
+    if (!measureWidthRef.current || !inputRef.current) {
+      return;
+    }
+
+    const mirrorWidth = measureWidthRef.current.offsetWidth;
+    inputRef.current.style.width = `${mirrorWidth}px`;
+  }, [value]);
+
+  return (
+    <>
+      <input {...register("text", { onChange })} {...props} ref={combinedRef} />
+      {/*input 내부 value size 측정용 span | Span for measuring the size of the value inside the input*/}
+      <span
+        aria-hidden={true}
+        ref={measureWidthRef}
+        className={"invisible absolute min-w-[1ch] whitespace-pre"}
+      >
+        {value || props.placeholder || ""}
+      </span>
+    </>
+  );
+}
+
+export { AutoResizeInput };
