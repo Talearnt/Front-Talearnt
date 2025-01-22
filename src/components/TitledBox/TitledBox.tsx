3deb1d36ca52a6ec4c36e5d5724b3875e4c2322c
ChoiBooKi <qnrl9034@naver.com>
2025-01-22 21:16:14 +0900

    [FEAT] TitledBox 열고 닫는 기능 추가 | Add open/close functionality TitledBox TALEARNT-332

--- src/components/TitledBox/TitledBox.tsx
+++ src/components/TitledBox/TitledBox.tsx
@@ -2,12 +2,15 @@ import { ReactNode } from "react";
 
 import { classNames } from "@utils/classNames";
 
+import { CaretIcon } from "@components/icons/CaretIcon/CaretIcon";
+
 type TitledBoxProps = {
   title: ReactNode;
   children: ReactNode;
+  canOpen?: boolean;
 };
 
-function TitledBox({ title, children }: TitledBoxProps) {
+function TitledBox({ title, children, canOpen }: TitledBoxProps) {
   return (
     <div
       className={classNames(
@@ -15,14 +18,40 @@ function TitledBox({ title, children }: TitledBoxProps) {
         "rounded-xl shadow-[inset_0_0_0_1px] shadow-talearnt-Line_01"
       )}
     >
-      <div
-        className={
-          "px-6 py-[19px] shadow-[inset_0_-1px_0_0] shadow-talearnt-Line_01"
-        }
+      <label
+        className={classNames(
+          "peer/label",
+          "flex items-center justify-between gap-4",
+          "px-6 py-[19px]",
+          "has-[:checked]:shadow-[inset_0_-1px_0_0] has-[:checked]:shadow-talearnt-Line_01",
+          canOpen && "cursor-pointer"
+        )}
       >
+        <input
+          className={"peer/checkbox hidden"}
+          type={"checkbox"}
+          checked={!canOpen ? true : undefined}
+          readOnly={!canOpen}
+        />
         {title}
+        {canOpen && (
+          <CaretIcon
+            className={classNames(
+              "stroke-talearnt-Icon_01",
+              "peer-checked/checkbox:rotate-180"
+            )}
+          />
+        )}
+      </label>
+      <div
+        className={classNames(
+          "hidden",
+          "px-6 py-8",
+          "peer-has-[:checked]/label:block"
+        )}
+      >
+        {children}
       </div>
-      <div className={"px-6 py-8"}>{children}</div>
     </div>
   );
 }
