7351610e706f0a8cfce61b45e8ba1759d1348ec1
ChoiBooKi <qnrl9034@naver.com>
2025-01-23 18:22:34 +0900

    [FEAT] TabSlider.tsx type 추가 | Add shadow type TabSlider.tsx TALEARNT-332

--- src/components/TabSlider/TabSlider.tsx
+++ src/components/TabSlider/TabSlider.tsx
@@ -1,59 +1,101 @@
+import { cva } from "class-variance-authority";
+
 import { classNames } from "@utils/classNames";
 
-type TabSliderProps = {
+import { CustomVariantProps } from "@common/common.type";
+
+type TabSliderVariantsType = Record<
+  "type",
+  Record<"default" | "shadow", string>
+>;
+
+type TabSliderProps = CustomVariantProps<TabSliderVariantsType> & {
   className?: string;
   currentValue: string;
   disabled?: boolean;
   onChangeHandler?: (value: string) => void;
-  onClickHandler?: (value: string) => void;
   options: { label: string; value: string }[];
 };
+
+const tabSliderVariants = cva<TabSliderVariantsType>(
+  classNames(
+    "flex items-center justify-center",
+    "w-full",
+    "cursor-pointer text-base font-medium",
+    "has-[:checked]:text-talearnt-Primary_01",
+    "has-[:disabled]:cursor-not-allowed"
+  ),
+  {
+    variants: {
+      type: {
+        default: classNames(
+          "shadow-[inset_0_0_0_1px] shadow-talearnt-Line_01",
+          "has-[:checked]:z-10 has-[:checked]:shadow-talearnt-Primary_01"
+        ),
+        shadow: classNames(
+          "px-[15px] py-[7px] rounded-full",
+          "border border-transparent",
+          "text-lg text-talearnt-Text_04",
+          "has-[:checked]:text-talearnt-Primary_01 has-[:checked]:bg-talearnt-BG_Background has-[:checked]:border-talearnt-Line_02 has-[:checked]:shadow-[0_0_20px_0_rgba(0,0,0,0.08)]"
+        )
+      }
+    },
+    defaultVariants: {
+      type: "default"
+    }
+  }
+);
+
+const defaultStyle = ({
+  disabled,
+  index,
+  maxLength
+}: {
+  disabled?: boolean;
+  index: number;
+  maxLength: number;
+}) =>
+  classNames(
+    disabled &&
+      "!text-talearnt-Text_04 has-[:checked]:bg-talearnt-BG_Up_01 has-[:checked]:shadow-talearnt-Line_01",
+    index === 0 ? "rounded-l-xl" : "-ml-px",
+    index === maxLength && "rounded-r-xl"
+  );
 
 function TabSlider({
   className,
   currentValue,
   disabled,
   onChangeHandler,
-  onClickHandler,
-  options
+  options,
+  type
 }: TabSliderProps) {
   return (
-    <div className={classNames("flex", className)}>
+    <div
+      className={classNames(
+        "flex",
+        type === "shadow" &&
+          "border-talearnt-Line_02 gap-4 rounded-full border bg-talearnt-BG_Up_01 p-[7px]",
+        className
+      )}
+    >
       {options.map(({ label, value }, index, array) => (
-        <div
+        <label
           className={classNames(
-            "group flex",
-            "h-[50px] w-full",
-            index > 0 && "-ml-px"
+            tabSliderVariants({ type }),
+            type !== "shadow" &&
+              defaultStyle({ disabled, index, maxLength: array.length - 1 })
           )}
-          key={`${value}-${index.toString()}`}
         >
           <input
             checked={currentValue === value}
-            className={"peer hidden"}
+            className={"hidden"}
             disabled={disabled}
-            id={`option${index.toString()}`}
-            name={"slider"}
-            onChange={() => onChangeHandler && onChangeHandler(value)}
-            onClick={() => onClickHandler && onClickHandler(value)}
+            onChange={() => onChangeHandler?.(value)}
             type={"radio"}
           />
-          <label
-            className={classNames(
-              "flex items-center justify-center",
-              "shadow-[inset_0_0_0_1px] shadow-talearnt-Line_01",
-              "w-full",
-              "cursor-pointer whitespace-nowrap text-base font-medium",
-              "peer-checked:relative peer-checked:z-10 peer-checked:text-talearnt-Primary_01 peer-checked:shadow-talearnt-Primary_01",
-              "peer-disabled:cursor-not-allowed peer-disabled:text-talearnt-Text_04 peer-disabled:peer-checked:bg-talearnt-BG_Up_01 peer-disabled:peer-checked:shadow-talearnt-Line_01",
-              index === 0 && "rounded-l-xl",
-              index === array.length - 1 && "rounded-r-xl"
-            )}
-            htmlFor={`option${index.toString()}`}
-          >
-            {label}
-          </label>
-        </div>
+          {label}
+        </label>
       ))}
     </div>
   );
