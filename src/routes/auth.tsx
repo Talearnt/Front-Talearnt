bfd53d62ed8faa597b19731962bca44b7a11d2a3
ChoiBooKi <qnrl9034@naver.com>
2025-01-31 20:59:47 +0900

    [CHORE] 폴더 변경 | Change folder

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -3,10 +3,10 @@ import { RouteObject } from "react-router-dom";
 // 공통
 import CompleteSignUp from "@pages/auth/components/CompleteSignUp/CompleteSignUp";
 // 계정 찾기
-import ChangePassword from "@pages/auth/FindAccount/components/ChangePassword/ChangePassword";
-import FindId from "@pages/auth/FindAccount/components/FindId/FindId";
-import FindPassword from "@pages/auth/FindAccount/components/FindPassword/FindPassword";
+import ChangePassword from "@pages/auth/FindAccount/ChangePassword/ChangePassword";
 import FindAccount from "@pages/auth/FindAccount/FindAccount";
+import FindId from "@pages/auth/FindAccount/FindId/FindId";
+import FindPassword from "@pages/auth/FindAccount/FindPassword/FindPassword";
 // 카카오
 import Kakao from "@pages/auth/Kakao/Kakao";
 import KakaoExtraInfo from "@pages/auth/Kakao/KakaoExtraInfo/KakaoExtraInfo";
@@ -14,8 +14,8 @@ import KakaoOauth from "@pages/auth/Kakao/KakaoOauth/KakaoOauth";
 // 로그인
 import SignIn from "@pages/auth/SignIn/SignIn";
 // 회원가입
-import Agreements from "@pages/auth/SignUp/components/Agreements/Agreements";
-import InfoFields from "@pages/auth/SignUp/components/InfoFields/InfoFields";
+import Agreements from "@pages/auth/SignUp/Agreements/Agreements";
+import InfoFields from "@pages/auth/SignUp/InfoFields/InfoFields";
 import SignUp from "@pages/auth/SignUp/SignUp";
 
 const authRouter: RouteObject[] = [
