58ad2fb69a59563cc4a2506b498758d6b14b8bf0
ChoiBooKi <qnrl9034@naver.com>
2025-02-07 23:49:01 +0900

    [FEAT] Chip 컴포넌트 수정 | Edit Chip component TALEARNT-333

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { MouseEvent, ReactNode } from "react";
 
 import { cva } from "class-variance-authority";
 
@@ -9,38 +9,50 @@ import { CloseIcon } from "@components/icons/CloseIcon/CloseIcon";
 import { CustomVariantProps } from "@common/common.type";
 
 type ChipVariantsType = {
-  type: Record<"default" | "keyword", string>;
+  type: Record<"default-small" | "default-large" | "keyword", string>;
 };
 
 type ChipProps = CustomVariantProps<ChipVariantsType> & {
   children: ReactNode;
-  onCloseHandler?: () => void;
+  onClickHandler?: (e: MouseEvent<HTMLDivElement>) => void;
+  onCloseHandler?: (e: MouseEvent<SVGSVGElement>) => void;
   pressed?: boolean;
 };
 
-const chipVariants = cva<ChipVariantsType>("", {
-  variants: {
-    type: {
-      default: "rounded-lg border border-talearnt_Icon_03 px-[15px] py-[10px]",
-      keyword: classNames(
-        "flex items-center gap-1",
-        "rounded-md px-2 py-[6px] bg-talearnt_BG_Up_02",
-        "text-body3_14_medium text-talearnt_Text_02"
-      )
+const chipVariants = cva<ChipVariantsType>(
+  "px-[15px] border border-talearnt_Icon_03 rounded-lg cursor-pointer",
+  {
+    variants: {
+      type: {
+        "default-small": "h-10 ",
+        "default-large": "h-[50px]",
+        keyword: classNames(
+          "flex items-center gap-1",
+          "rounded-md px-2 py-[6px] bg-talearnt_BG_Up_02 border-none cursor-default",
+          "text-body3_14_medium text-talearnt_Text_02"
+        )
+      }
+    },
+    defaultVariants: {
+      type: "default-small"
     }
-  },
-  defaultVariants: {
-    type: "default"
   }
-});
+);
 
-function Chip({ children, onCloseHandler, pressed, type }: ChipProps) {
+function Chip({
+  children,
+  onClickHandler,
+  onCloseHandler,
+  pressed,
+  type
+}: ChipProps) {
   return (
     <div
       className={classNames(
         chipVariants({ type }),
         pressed && "border-talearnt_Primary_01"
       )}
+      onClick={onClickHandler}
     >
       {type === "keyword" ? (
         <>
@@ -52,7 +64,9 @@ function Chip({ children, onCloseHandler, pressed, type }: ChipProps) {
       ) : (
         <span
           className={classNames(
-            "text-body3_14_medium text-talearnt_Text_04",
+            "text-talearnt_Text_04",
+            type === "default-small" && "text-body3_14_medium",
+            type === "default-large" && "text-body2_16_medium",
             pressed && "text-talearnt_Primary_01"
           )}
         >
