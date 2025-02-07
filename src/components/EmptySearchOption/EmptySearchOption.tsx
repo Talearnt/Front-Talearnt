fe1c83bc86b02ba65dc1dda145732c13f9a19299
ChoiBooKi <qnrl9034@naver.com>
2025-02-08 00:08:43 +0900

    [FEAT] EmptySearchOption 개발 | Make EmptySearchOption TALEARNT-333

---
+++ src/components/EmptySearchOption/EmptySearchOption.tsx
@@ -0,0 +1,19 @@
+import { MakoExpressionSad } from "@components/icons/mako/MakoExpressionSad";
+
+function EmptySearchOption({ search }: { search: string }) {
+  return (
+    <>
+      <p className="w-full text-center text-heading4_20_semibold text-talearnt_Text_03">
+        <span className="inline-block max-w-[calc(100%-25px)] overflow-hidden text-ellipsis whitespace-nowrap align-sub">
+          '{search}
+        </span>
+        '에
+        <br />
+        대한 검색 결과가 없어요...
+      </p>
+      <MakoExpressionSad />
+    </>
+  );
+}
+
+export { EmptySearchOption };
