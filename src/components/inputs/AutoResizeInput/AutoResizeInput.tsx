c06bb1e3d628f61f93f7a63f5465f8b14168a18f
ChoiBooKi <qnrl9034@naver.com>
2025-02-09 21:40:53 +0900

    [FEAT] AutoResizeInput.tsx 수정 | Edit AutoResizeInput.tsx TALEARNT-333

--- src/components/inputs/AutoResizeInput/AutoResizeInput.tsx
+++ src/components/inputs/AutoResizeInput/AutoResizeInput.tsx
@@ -1,17 +1,24 @@
 import { ComponentProps, useEffect, useRef } from "react";
-import { useForm } from "react-hook-form";
+import { UseFormRegisterReturn } from "react-hook-form/dist/types/form";
 
-function AutoResizeInput({ onChange, ...props }: ComponentProps<"input">) {
+type AutoResizeInputProps = ComponentProps<"input"> & {
+  formData?: UseFormRegisterReturn;
+};
+
+function AutoResizeInput({
+  placeholder,
+  value,
+  formData,
+  ...props
+}: AutoResizeInputProps) {
   const measureWidthRef = useRef<HTMLSpanElement>(null);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const { register, watch } = useForm<{ text: string }>();
-
-  const value = watch("text");
-
   const combinedRef = (node: HTMLInputElement | null) => {
     inputRef.current = node;
-    register("text").ref(node);
+    if (formData?.ref) {
+      formData.ref(node);
+    }
   };
 
   useEffect(() => {
@@ -19,20 +26,24 @@ function AutoResizeInput({ onChange, ...props }: ComponentProps<"input">) {
       return;
     }
 
-    const mirrorWidth = measureWidthRef.current.offsetWidth;
-    inputRef.current.style.width = `${mirrorWidth.toString()}px`;
-  }, [value]);
+    inputRef.current.style.width = `${measureWidthRef.current.offsetWidth.toString()}px`;
+  }, [value, placeholder]);
 
   return (
     <>
-      <input {...register("text", { onChange })} {...props} ref={combinedRef} />
+      <input
+        placeholder={placeholder}
+        {...formData}
+        {...props}
+        ref={combinedRef}
+      />
       {/*input 내부 value size 측정용 span | Span for measuring the size of the value inside the input*/}
       <span
         aria-hidden={true}
         ref={measureWidthRef}
         className={"invisible absolute min-w-[1ch] whitespace-pre"}
       >
-        {value || props.placeholder || ""}
+        {value || placeholder || ""}
       </span>
     </>
   );
