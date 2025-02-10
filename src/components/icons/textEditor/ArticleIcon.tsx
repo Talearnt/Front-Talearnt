876db82b15bee75e0d3b06abe6a7741a91f3c0a3
ChoiBooKi <qnrl9034@naver.com>
2025-02-10 21:56:20 +0900

    [FEAT] 게시판용 아이콘 개발 | Make icons for text editor TALEARNT-333

---
+++ src/components/icons/textEditor/ArticleIcon.tsx
@@ -0,0 +1,22 @@
+function ArticleIcon() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="32" height="32" rx="8" fill="#FF5C5C" />
+      <path
+        d="M6.39998 8.20312L25.6 8.20312M6.39998 13.4031L20.5241 13.4031M6.39998 18.6031L25.6 18.6031M6.39998 23.8031L16.662 23.8031"
+        stroke="white"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export { ArticleIcon };
