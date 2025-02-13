183ec89c6c5b114a719d5d4ae9771fe3d4698da0
ChoiBooKi <qnrl9034@naver.com>
2025-02-14 00:44:54 +0900

    [FEAT] WriteArticleInfo props 변경 | Change WriteArticleInfo props TALEARNT-333

--- src/pages/articles/WriteArticle/core/writeArticle.type.ts
+++ src/pages/articles/WriteArticle/core/writeArticle.type.ts
@@ -2,52 +2,46 @@ import { dropdownOptionType } from "@components/dropdowns/dropdown.type";
 
 // 게시글 타입
 type articleType = "match" | "community";
-
 // 게시글 데이터
 type commonArticleDataType = {
   title: string;
   content: string;
+  pureText: string;
+  imageUrls: string[];
 };
-
-// 커뮤니티 게시글 - 게시판 타입
-type boardType = "자유 게시판" | "질문 게시판" | "스터디 모집";
-
 // 매칭 게시글 - 진행 기간
 type durationType = "기간 미정" | "1개월" | "2개월" | "3개월" | "3개월 이상";
-
 // 매칭 게시글 - 진행 방식
 type exchangeType = "온라인" | "오프라인" | "온오프라인";
-
 // 매칭 게시글 - state
 type matchArticleFormDataType = commonArticleDataType & {
   duration: dropdownOptionType<durationType>[];
   exchangeType: exchangeType;
-  imageUrls: string[];
   giveTalents: dropdownOptionType<number>[];
   receiveTalents: dropdownOptionType<number>[];
 };
-
 // 매칭 게시글 - body
-type matchArticleBodyType = Omit<
+type matchArticleBodyType = Pick<
   matchArticleFormDataType,
-  "duration" | "giveTalents" | "receiveTalents"
+  "title" | "content" | "imageUrls" | "exchangeType"
 > & {
   duration: durationType;
   giveTalents: number[];
   receiveTalents: number[];
 };
-
-// 커뮤니티 게시글 - Body
+// 커뮤니티 게시글 - 게시판 타입
+type postType = "자유 게시판" | "질문 게시판" | "스터디 모집";
+// 커뮤니티 게시글 - state
 type communityArticleDataType = commonArticleDataType & {
-  boardType: string;
+  postType: postType;
 };
 
 export type {
   articleType,
-  boardType,
   communityArticleDataType,
   durationType,
   exchangeType,
   matchArticleBodyType,
-  matchArticleFormDataType
+  matchArticleFormDataType,
+  postType
 };
