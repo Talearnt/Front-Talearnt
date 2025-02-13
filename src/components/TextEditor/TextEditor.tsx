85f9e5601141e3fd2216c7fcbb137d80f7ba9f58
ChoiBooKi <qnrl9034@naver.com>
2025-02-14 00:20:40 +0900

    [FEAT] 에러 메시지 적용 | Apply error message TALEARNT-333

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -4,6 +4,7 @@ import ReactQuill from "react-quill-new";
 
 import { classNames } from "@utils/classNames";
 
+import { ErrorIcon } from "@components/icons/ErrorIcon/ErrorIcon";
 import { Toolbar } from "@components/TextEditor/Toolbar/Toolbar";
 
 import "react-quill-new/dist/quill.snow.css";
@@ -12,13 +13,21 @@ type TextEditorProps = {
   value: string;
   onChangeHandler: (value: string) => void;
   editorKey?: string;
+  error?: string;
 };
 
-function TextEditor({ value, onChangeHandler, editorKey }: TextEditorProps) {
+function TextEditor({
+  value,
+  onChangeHandler,
+  editorKey,
+  error
+}: TextEditorProps) {
   const quillRef = useRef<ReactQuill>(null);
 
   const [isExpanded, setIsExpanded] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
+
+  const hasError = !!error;
 
   useEffect(() => {
     const handleExpanded = () => {
@@ -31,37 +40,48 @@ function TextEditor({ value, onChangeHandler, editorKey }: TextEditorProps) {
   }, []);
 
   return (
-    <div
-      className={classNames(
-        "flex flex-col",
-        "overflow-hidden rounded-xl border border-talearnt_Line_01",
-        (isFocused || isExpanded) && "border-talearnt_Primary_01"
-      )}
-    >
-      <Toolbar />
-      <div className={classNames("flex flex-col", "p-6 pb-[23px]")}>
-        <ReactQuill
-          ref={quillRef}
-          modules={{
-            toolbar: {
-              container: "#toolbar"
-            }
-          }}
-          value={value}
-          onChange={onChangeHandler}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          key={editorKey}
-        />
-        <span
-          className={classNames(
-            "ml-auto",
-            "text-body2_16_medium text-talearnt_Text_04"
-          )}
-        >
-          {(quillRef.current?.editor?.getLength() ?? 1) - 1}/1000
-        </span>
+    <div className={"flex flex-col"}>
+      <div
+        className={classNames(
+          "flex flex-col",
+          "overflow-hidden rounded-xl border border-talearnt_Line_01",
+          (isFocused || isExpanded) && "border-talearnt_Primary_01",
+          hasError && "border-talearnt_Error_01"
+        )}
+      >
+        <Toolbar />
+        <div className={classNames("flex flex-col", "p-6 pb-[23px]")}>
+          <ReactQuill
+            ref={quillRef}
+            modules={{
+              toolbar: {
+                container: "#toolbar"
+              }
+            }}
+            value={value}
+            onChange={onChangeHandler}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            key={editorKey}
+          />
+          <span
+            className={classNames(
+              "ml-auto",
+              "text-body2_16_medium text-talearnt_Text_04"
+            )}
+          >
+            {(quillRef.current?.editor?.getLength() ?? 1) - 1}/1000
+          </span>
+        </div>
       </div>
+      {hasError && (
+        <div className={"mt-1 flex items-center gap-1"}>
+          <ErrorIcon />
+          <span className={"text-caption1_14_medium text-talearnt_Error_01"}>
+            {error}
+          </span>
+        </div>
+      )}
     </div>
   );
 }
