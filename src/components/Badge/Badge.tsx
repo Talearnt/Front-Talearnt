73f94412b19dbe675ffc0fb90df29eaf91ed8554
ChoiBooKi <qnrl9034@naver.com>
2025-02-15 09:27:42 +0900

    [FEAT] Badge 컴포넌트 개발 | Make Badge component TALEARNT-333

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,42 @@
+import { cva } from "class-variance-authority";
+
+import { CustomVariantProps } from "@common/common.type";
+
+type BadgeVariantsType = {
+  type: Record<
+    "primary" | "disabled" | "default" | "error" | "primary-border",
+    string
+  >;
+  size: Record<"small" | "medium", string>;
+};
+
+type BadgeProps = CustomVariantProps<BadgeVariantsType> & {
+  label: string;
+};
+
+const badgeVariants = cva<BadgeVariantsType>("rounded-full", {
+  variants: {
+    type: {
+      primary: "bg-talearnt_PrimaryBG_04 text-talearnt_Primary_01",
+      disabled: "bg-talearnt_BG_Up_01 text-talearnt_Text_04",
+      default: "bg-talearnt_BG_Up_01 text-talearnt_Text_02",
+      error: "bg-talearnt_BG_Badge_01 text-talearnt_Text_Red",
+      "primary-border":
+        "shadow-[inset_0_0_0_1px] shadow-talearnt_PrimaryBG_03 bg-talearnt_PrimaryBG_01 text-talearnt_Primary_01"
+    },
+    size: {
+      small: "px-2 py-1 text-caption2_12_semibold",
+      medium: "px-2 py-[6px] text-body3_14_medium"
+    }
+  },
+  defaultVariants: {
+    type: "primary",
+    size: "small"
+  }
+});
+
+function Badge({ label, type, size }: BadgeProps) {
+  return <span className={badgeVariants({ type, size })}>{label}</span>;
+}
+
+export { Badge };
