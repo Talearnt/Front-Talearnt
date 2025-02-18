620f152c3e17056b54309bccea844d6cb5c97458
ChoiBooKi <qnrl9034@naver.com>
2025-02-18 20:24:03 +0900

    [FEAT] 페이지네이션 type 추가 | Add pagination type TALEARNT-373

--- src/common/common.type.ts
+++ src/common/common.type.ts
@@ -1,22 +1,38 @@
 import { ComponentProps } from "react";
 
-export type CustomVariantProps<
-  T extends Record<string, Record<string, string>>
-> = {
+type CustomVariantProps<T extends Record<string, Record<string, string>>> = {
   [K in keyof T]?: keyof T[K];
 };
 
-export type CommonIconProps = ComponentProps<"svg"> & {
+type CommonIconProps = ComponentProps<"svg"> & {
   size?: number;
 };
 
-export type responseDataType<T> = {
+type responseDataType<T> = {
   data: T;
   errorCode: string | null;
   errorMessage: string | null;
   success: boolean;
 };
 
-export type customAxiosResponseType<T> = responseDataType<T> & {
+type customAxiosResponseType<T> = responseDataType<T> & {
   status: number;
+};
+
+type paginationType<T> = {
+  data: T[];
+  pagination: {
+    hasNext: boolean;
+    hasPrevious: boolean;
+    totalPages: number;
+    currentPage: number;
+  };
+};
+
+export type {
+  CommonIconProps,
+  customAxiosResponseType,
+  CustomVariantProps,
+  paginationType,
+  responseDataType
 };
