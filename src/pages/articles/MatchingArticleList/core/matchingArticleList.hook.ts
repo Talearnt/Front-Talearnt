cbaabdbf6bb50566661d41f712fdc710ad9fbbb8
ChoiBooKi <qnrl9034@naver.com>
2025-02-24 22:14:05 +0900

    [FEAT] 매칭 게시물 목록 필터 개발 | Make filter to matching article list TALEARNT-373

--- src/pages/articles/MatchingArticleList/core/matchingArticleList.hook.ts
+++ src/pages/articles/MatchingArticleList/core/matchingArticleList.hook.ts
@@ -1,10 +1,26 @@
+import { useShallow } from "zustand/shallow";
+
 import { getMatchingArticleList } from "@pages/articles/MatchingArticleList/core/matchingArticleList.api";
 
 import { createQueryKey } from "@utils/queryKey";
 
 import { useQueryWithInitial } from "@hook/useQueryWithInitial";
 
+import useFilterStore from "@pages/articles/MatchingArticleList/core/matchingArticleList.store";
+
 export const useGetMatchingArticleList = () => {
+  const filter = useFilterStore(
+    useShallow(state => ({
+      giveTalents: state.giveTalents,
+      receiveTalents: state.receiveTalents,
+      duration: state.duration,
+      type: state.type,
+      status: state.status,
+      order: state.order,
+      page: state.page
+    }))
+  );
+
   return useQueryWithInitial(
     {
       results: [],
@@ -16,8 +32,8 @@ export const useGetMatchingArticleList = () => {
       }
     },
     {
-      queryKey: createQueryKey(["MATCH"], { isArticleList: true }),
-      queryFn: async () => await getMatchingArticleList()
+      queryKey: createQueryKey(["MATCH", filter], { isArticleList: true }),
+      queryFn: async () => await getMatchingArticleList(filter)
     }
   );
 };
