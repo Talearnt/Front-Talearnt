18ab10110820b3599e4824deccd7995b685c5538
ChoiBooKi <qnrl9034@naver.com>
2025-02-25 21:32:12 +0900

    [CHORE] export 방식 수정 | Modify export

--- src/components/TextEditor/Toolbar/toolbar.constants.ts
+++ src/components/TextEditor/Toolbar/toolbar.constants.ts
@@ -1,7 +1,7 @@
-type attributorsType = {
+export type attributorsType = {
   whitelist: string[];
 };
-type quillIconsType = {
+export type quillIconsType = {
   bold: string | null;
   italic: string | null;
   underline: string | null;
@@ -12,8 +12,8 @@ type quillIconsType = {
   image: string | null;
 };
 
-const alignOptions = ["", "center", "right"] as const;
-const backgroundColorOptions = [
+export const alignOptions = ["", "center", "right"] as const;
+export const backgroundColorOptions = [
   "#FFE5E5",
   "#FFE2C2",
   "#FFF5C2",
@@ -23,7 +23,7 @@ const backgroundColorOptions = [
   "#DEE1E3",
   "#FFFFFF"
 ];
-const colorOptions = [
+export const colorOptions = [
   "#FF2727",
   "#FF9A27",
   "#FFDB27",
@@ -32,18 +32,18 @@ const colorOptions = [
   "#A927FF",
   "#1E2224"
 ];
-const DEFAULT_VALUE = {
+export const DEFAULT_VALUE = {
   size: "16px" as (typeof sizeOptions)[number],
   color: "#1E2224" as (typeof colorOptions)[number],
   backgroundColor: "#FFFFFF" as (typeof backgroundColorOptions)[number]
 };
-const iconsOptions = {
+export const iconsOptions = {
   background:
     '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.1226 16.1984V7.79844M12.1226 7.79844H8.99844M12.1226 7.79844H14.9984M4.79844 21.5984H19.1984C20.5239 21.5984 21.5984 20.5239 21.5984 19.1984V4.79844C21.5984 3.47295 20.5239 2.39844 19.1984 2.39844H4.79844C3.47295 2.39844 2.39844 3.47295 2.39844 4.79844V19.1984C2.39844 20.5239 3.47295 21.5984 4.79844 21.5984Z" class="ql-stroke ql-fill ql-color-label" /></svg>',
   color:
     '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4.25 4.70886C4.25 4.31737 4.5735 4 4.97257 4H15.5274C15.9265 4 16.25 4.31737 16.25 4.70886C16.25 5.10035 15.9265 5.41772 15.5274 5.41772H11.1912V19.2911C11.1912 19.6826 10.8677 20 10.4686 20C10.0695 20 9.74602 19.6826 9.74602 19.2911V5.41772H4.97257C4.5735 5.41772 4.25 5.10035 4.25 4.70886Z" class="ql-stroke fill"/><rect x="14.25" y="14" width="6" height="6" rx="3" class="ql-color-label" fill="#1E2224"/></svg>'
 };
-const sizeOptions = [
+export const sizeOptions = [
   "12px",
   "14px",
   "16px",
@@ -54,7 +54,7 @@ const sizeOptions = [
   "30px"
 ];
 
-const updatePickerItemColors = (selector: string) => {
+export const updatePickerItemColors = (selector: string) => {
   document.querySelectorAll(`${selector} .ql-picker-item`).forEach(item => {
     const color = item.getAttribute("data-value");
 
@@ -63,14 +63,3 @@ const updatePickerItemColors = (selector: string) => {
     }
   });
 };
-
-export {
-  alignOptions,
-  backgroundColorOptions,
-  colorOptions,
-  DEFAULT_VALUE,
-  iconsOptions,
-  sizeOptions,
-  updatePickerItemColors
-};
-export type { attributorsType, quillIconsType };
