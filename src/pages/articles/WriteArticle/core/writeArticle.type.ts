610c92a364c6cc8ac3106a9961b1a3042e025394
ChoiBooKi <qnrl9034@naver.com>
2025-03-06 21:11:12 +0900

    [FEAT] 타입 추가 및 수정 | Modify & Add type TALEARNT-391

--- src/pages/articles/WriteArticle/core/writeArticle.type.ts
+++ src/pages/articles/WriteArticle/core/writeArticle.type.ts
@@ -1,6 +1,6 @@
 import {
   communityArticleBodyType,
-  matchArticleBodyType
+  matchingArticleBodyType
 } from "@pages/articles/core/articles.type";
 
 // 게시글 타입
@@ -17,16 +17,21 @@ export type imageFileType = {
 export type presignedURLBodyType = Omit<imageFileType, "file" | "url">;
 
 // 매칭 게시글 - state
-export type matchArticleFormDataType = Omit<
-  matchArticleBodyType,
+export type matchingArticleFormDataType = Omit<
+  matchingArticleBodyType,
   "imageUrls"
 > & {
   pureText: string;
   imageFileList: imageFileType[];
 };
 
+// 매칭 게시글 수정
+export type editMatchingArticleDataType = matchingArticleFormDataType & {
+  exchangePostNo: number;
+};
+
 // 커뮤니티 게시글 - state
-export type communityArticleDataType = Omit<
+export type communityArticleFormDataType = Omit<
   communityArticleBodyType,
   "imageUrls"
 > & {
