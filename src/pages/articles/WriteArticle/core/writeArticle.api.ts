c78eed3ee6820720292232fa5bfb9d39f5d25fa6
ChoiBooKi <qnrl9034@naver.com>
2025-03-09 20:56:48 +0900

    [FEAT] 매칭 게시물 작성 hook 적용 | Apply write matching article hook TALEARNT-391

--- src/pages/articles/WriteArticle/core/writeArticle.api.ts
+++ src/pages/articles/WriteArticle/core/writeArticle.api.ts
@@ -1,11 +1,20 @@
-import { postAPI } from "@utils/apiMethods";
+import { postAPI, putAPI } from "@utils/apiMethods";
 
-import { matchArticleBodyType } from "@pages/articles/core/articles.type";
+import { matchingArticleBodyType } from "@pages/articles/core/articles.type";
 import { presignedURLBodyType } from "@pages/articles/WriteArticle/core/writeArticle.type";
 
-// TODO 커뮤니티용 body 가져오기
-export const postArticle = async (body: matchArticleBodyType) =>
-  await postAPI("/v1/posts/exchanges", body, { withCredentials: true });
+export const postMatchingArticle = async (body: matchingArticleBodyType) =>
+  await postAPI<number>("/v1/posts/exchanges", body, { withCredentials: true });
+
+export const putMatchingArticle = async ({
+  exchangePostNo,
+  ...body
+}: matchingArticleBodyType & {
+  exchangePostNo: number;
+}) =>
+  await putAPI(`/v1/posts/exchanges/${exchangePostNo}`, body, {
+    withCredentials: true
+  });
 
 export const postToGetPresignedURL = async (files: presignedURLBodyType[]) =>
   await postAPI<string[]>("/v1/uploads", files, { withCredentials: true });
