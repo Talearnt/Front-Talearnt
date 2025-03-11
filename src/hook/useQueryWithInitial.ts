4070c56c3010c9aeb7ca4149d1abc3d1b198d215
ChoiBooKi <qnrl9034@naver.com>
2025-03-12 00:02:41 +0900

    [FEAT] 게시물 상세 페이지 로딩, 에러 적용 | Apply MatchingArticleDetail page loading, error TALEARNT-391

--- src/hook/useQueryWithInitial.ts
+++ src/hook/useQueryWithInitial.ts
@@ -1,18 +1,28 @@
 import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import type { UseQueryResult } from "@tanstack/react-query/src/types";
 
 import { customAxiosResponseType } from "@common/common.type";
 
 export const useQueryWithInitial = <T>(
   initialData: T,
-  options: UseQueryOptions<customAxiosResponseType<T>>
-): {
+  options: UseQueryOptions<
+    customAxiosResponseType<T>,
+    customAxiosResponseType<null>
+  >
+): Omit<
+  UseQueryResult<customAxiosResponseType<T>, customAxiosResponseType<null>>,
+  "isLoading"
+> & {
   data: customAxiosResponseType<T>;
   isError: boolean;
   isLoading: boolean;
   isSuccess: boolean;
   status: "error" | "pending" | "success";
 } => {
-  const { data, ...value } = useQuery(options);
+  const { data, ...value } = useQuery<
+    customAxiosResponseType<T>,
+    customAxiosResponseType<null>
+  >(options);
 
   const isSuccess = value.isSuccess && value.fetchStatus === "idle";
   const isLoading = value.isLoading || value.isFetching;
