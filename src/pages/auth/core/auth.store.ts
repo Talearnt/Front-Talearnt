5575e401c6cc3415eb5f65c276f28d08fa2d1770
ChoiBooKi <qnrl9034@naver.com>
2025-03-11 23:57:36 +0900

    [FEAT] 게시물 작성 페이지 로딩 적용 | Apply WriteArticle loading TALEARNT-391

--- src/pages/auth/core/auth.store.ts
+++ src/pages/auth/core/auth.store.ts
@@ -86,9 +86,12 @@ export const useKakaoAuthResponseStore = create<kakaoAuthResponseStoreType>(
 type authStoreType = {
   accessToken: string | null;
   setAccessToken: (token: string | null) => void;
+  isLoggedIn: boolean;
 };
 
 export const useAuthStore = create<authStoreType>(set => ({
   accessToken: null,
-  setAccessToken: token => set({ accessToken: token })
+  setAccessToken: token =>
+    set({ accessToken: token, isLoggedIn: token !== null }),
+  isLoggedIn: false
 }));
