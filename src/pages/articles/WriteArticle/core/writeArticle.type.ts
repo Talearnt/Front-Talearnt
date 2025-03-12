bb25a3598828d883d7e6bd39d64f48f8a80ad46c
ChoiBooKi <qnrl9034@naver.com>
2025-03-12 21:00:12 +0900

    [CHORE] 타입 수정 | type 수정 TALEARNT-402

--- src/pages/articles/WriteArticle/core/writeArticle.type.ts
+++ src/pages/articles/WriteArticle/core/writeArticle.type.ts
@@ -1,14 +1,13 @@
 import {
   commonArticleDataType,
-  communityArticleBodyType,
   durationType,
-  exchangeType
+  exchangeType,
+  postType
 } from "@pages/articles/core/articles.type";
 import { matchingArticleDetailType } from "@pages/articles/MatchingArticleDetail/core/matchingArticleDetail.type";
 
 // 게시글 타입
 export type articleType = "match" | "community";
-
 // 이미지 파일
 export type imageFileType = {
   file: File;
@@ -20,14 +19,16 @@ export type imageFileType = {
 export type presignedURLBodyType = Omit<imageFileType, "file" | "url">;
 
 // 매칭 게시글 - body
-export type matchingArticleBodyType = commonArticleDataType & {
+export type matchingArticleBodyType = Pick<
+  commonArticleDataType,
+  "title" | "content"
+> & {
   duration: durationType;
   exchangeType: exchangeType;
   giveTalents: number[];
   receiveTalents: number[];
   imageUrls: string[];
 };
-
 // 매칭 게시글 - state
 export type matchingArticleFormDataType = Omit<
   matchingArticleBodyType,
@@ -36,11 +37,18 @@ export type matchingArticleFormDataType = Omit<
   pureText: string;
   imageFileList: imageFileType[];
 };
-
 // 매칭 게시글 수정
 export type editMatchingArticleDataType = matchingArticleFormDataType &
   Pick<matchingArticleDetailType, "exchangePostNo">;
 
+// 커뮤니티 게시글 - body
+export type communityArticleBodyType = Pick<
+  commonArticleDataType,
+  "title" | "content"
+> & {
+  postType: postType;
+  imageUrls: string[];
+};
 // 커뮤니티 게시글 - state
 export type communityArticleFormDataType = Omit<
   communityArticleBodyType,
