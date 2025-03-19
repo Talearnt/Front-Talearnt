5beecac0eb9fc13da6739f3b5aac2b394607428e
ChoiBooKi <qnrl9034@naver.com>
2025-03-19 21:26:47 +0900

    [FEAT] 댓글/답글 타입 추가 | Add comment type TALEARNT-402

--- src/pages/articles/core/articles.type.ts
+++ src/pages/articles/core/articles.type.ts
@@ -1,8 +1,13 @@
 // 게시글 데이터
-export type commonArticleDataType = {
-  createdAt: string;
-  title: string;
+import { profileType } from "@type/user.type";
+
+type baseContentType = {
   content: string;
+  createdAt: string;
+  updatedAt: string;
+};
+export type commonArticleDataType = baseContentType & {
+  title: string;
 };
 // 매칭 게시글 - 진행 기간
 export type durationType =
@@ -15,3 +20,40 @@ export type durationType =
 export type exchangeType = "온라인" | "오프라인" | "온/오프라인";
 // 커뮤니티 게시글 - 게시판 타입
 export type postType = "자유 게시판" | "질문 게시판" | "스터디 모집 게시판";
+// 댓글
+export type commentType = Pick<
+  profileType,
+  "userNo" | "nickname" | "profileImg"
+> &
+  baseContentType & {
+    commentNo: number;
+    replyCount: number;
+  };
+// {
+//   "content": "",
+//   "commentNo": 0,
+//   "createdAt": "",
+//   "nickname": "",
+//   "profileImg": null,
+//   "replyCount": 0,
+//   "updatedAt": "",
+//   "userNo": 0
+// }
+
+// 답글
+export type replyType = Pick<
+  profileType,
+  "userNo" | "nickname" | "profileImg"
+> &
+  baseContentType & {
+    replyNo: number;
+  };
+// {
+//   "content": "",
+//   "createdAt": "",
+//   "nickname": "",
+//   "profileImg": null,
+//   "replyNo": 0,
+//   "updatedAt": "",
+//   "userNo": 0
+// }
