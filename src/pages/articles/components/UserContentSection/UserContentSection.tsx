1a29d85b732ac2cc58c403d30d4354fcd0ff11ba
ChoiBooKi <qnrl9034@naver.com>
2025-03-27 21:28:47 +0900

    [FEAT] UserContentSection 컴포넌트 개발 | Make UserContentSection component TALEARNT-402

---
+++ src/pages/articles/components/UserContentSection/UserContentSection.tsx
@@ -0,0 +1,74 @@
+import { ReactNode } from "react";
+
+import dayjs from "dayjs";
+
+import { classNames } from "@utils/classNames";
+
+import { useGetProfile } from "@hook/user.hook";
+
+import { Avatar } from "@components/Avatar/Avatar";
+
+import { profileType } from "@type/user.type";
+
+type UserContentSectionProps = Pick<profileType, "profileImg"> & {
+  authorNickname: string;
+  createdAt: string;
+  content: string;
+  children?: ReactNode;
+};
+
+function UserContentSection({
+  profileImg,
+  createdAt,
+  authorNickname,
+  content,
+  children
+}: UserContentSectionProps) {
+  const {
+    data: {
+      data: { nickname }
+    }
+  } = useGetProfile();
+
+  return (
+    <div className={"flex gap-4"}>
+      <Avatar imageUrl={profileImg} />
+      <div className={classNames("flex flex-col gap-2", "w-full")}>
+        <div className={"flex items-center gap-2"}>
+          <span className={"text-body2_16_semibold text-talearnt_Text_01"}>
+            {authorNickname}
+          </span>
+          {authorNickname === nickname && (
+            <span
+              className={classNames(
+                "rounded-full border border-talearnt_Error_01 px-[7px] py-[3px]",
+                "text-caption2_12_semibold text-talearnt_Error_01"
+              )}
+            >
+              작성자
+            </span>
+          )}
+          <div className={"mx-2 h-[20px] w-px bg-talearnt_Line_01"} />
+          <time
+            className={"text-caption1_14_medium text-talearnt_Text_03"}
+            dateTime={dayjs(createdAt).format("YYYY.MM.DD")}
+          >
+            {dayjs(createdAt).format("YYYY.MM.DD")}
+          </time>
+          <time
+            className={"text-caption1_14_medium text-talearnt_Text_03"}
+            dateTime={dayjs(createdAt).format("HH:MM")}
+          >
+            {dayjs(createdAt).format("HH:MM")}
+          </time>
+        </div>
+        <p className={"text-body2_16_medium text-talearnt_Text_02"}>
+          {content}
+        </p>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export { UserContentSection };
