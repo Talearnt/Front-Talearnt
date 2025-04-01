31a9d8695f31cc065d57204568f4bedb4b4f1ec2
ChoiBooKi <qnrl9034@naver.com>
2025-04-01 22:00:11 +0900

    [FEAT] Textarea 수정 | Edit Textarea TALEARNT-402

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,54 +1,46 @@
-import { ChangeEvent, ComponentProps, useRef, useState } from "react";
+import { ChangeEvent, ComponentProps, forwardRef } from "react";
 
 import { classNames } from "@utils/classNames";
 
-function Textarea({
-  className,
-  onChange,
-  maxLength,
-  ...props
-}: ComponentProps<"textarea">) {
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+const Textarea = forwardRef<HTMLTextAreaElement, ComponentProps<"textarea">>(
+  ({ className, onChange, maxLength, value, ...props }, ref) => {
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      const textarea = e.target;
+      // 높이 자동 조절
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+      onChange?.(e);
+    };
 
-  const [value, setValue] = useState("");
-
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const textarea = e.target;
-
-    setValue(textarea.value);
-    // 높이 자동 조절
-    textarea.style.height = "auto";
-    textarea.style.height = `${textarea.scrollHeight}px`;
-    onChange?.(e);
-  };
-
-  return (
-    <div
-      className={classNames(
-        "flex",
-        "w-full rounded-lg border border-talearnt_Line_01 bg-talearnt_BG_Background px-[15px] py-[11px]",
-        "focus-within:border-talearnt_Primary_01 hover:border-talearnt_Primary_01",
-        className
-      )}
-    >
-      <textarea
-        ref={textareaRef}
-        rows={1}
+    return (
+      <div
         className={classNames(
-          "flex-1",
-          "text-body2_16_medium text-talearnt_Text_02",
-          "resize-none placeholder:text-talearnt_Text_04 focus:outline-none"
+          "flex",
+          "w-full rounded-lg border border-talearnt_Line_01 bg-talearnt_BG_Background px-[15px] py-[11px]",
+          "focus-within:border-talearnt_Primary_01 hover:border-talearnt_Primary_01",
+          className
         )}
-        {...props}
-        onChange={handleChange}
-      />
-      {maxLength !== undefined && (
-        <span className={"text-body2_16_medium text-talearnt_Text_04"}>
-          {value.length}/{maxLength}
-        </span>
-      )}
-    </div>
-  );
-}
+      >
+        <textarea
+          ref={ref}
+          rows={1}
+          className={classNames(
+            "flex-1",
+            "text-body2_16_medium text-talearnt_Text_02",
+            "resize-none placeholder:text-talearnt_Text_04 focus:outline-none"
+          )}
+          value={value}
+          {...props}
+          onChange={handleChange}
+        />
+        {maxLength !== undefined && (
+          <span className={"text-body2_16_medium text-talearnt_Text_04"}>
+            {String(value).length}/{maxLength}
+          </span>
+        )}
+      </div>
+    );
+  }
+);
 
 export { Textarea };
