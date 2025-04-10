cf07fecc6b84472fdd192cb5d9f8dac4dd2ad4c8
ChoiBooKi <qnrl9034@naver.com>
2025-04-10 23:12:11 +0900

    [FEAT] 댓글/답글 작성, 수정, 삭제 API 추가 | Add API for comment/reply create, update, delete TALEARNT-402

--- src/pages/articles/CommunityArticleDetail/core/communityArticleDetail.api.ts
+++ src/pages/articles/CommunityArticleDetail/core/communityArticleDetail.api.ts
@@ -1,8 +1,12 @@
-import { deleteAPI, getAPI } from "@utils/apiMethods";
+import { deleteAPI, getAPI, postAPI, putAPI } from "@utils/apiMethods";
 
 import { paginationType } from "@common/common.type";
 import { communityArticleDetailType } from "@pages/articles/CommunityArticleDetail/core/communityArticleDetail.type";
-import { commentType, replyType } from "@pages/articles/core/articles.type";
+import {
+  baseContentType,
+  commentType,
+  replyType
+} from "@pages/articles/core/articles.type";
 
 // 커뮤니티 게시물 상세 정보 조회
 export const getCommunityArticleDetail = async (communityPostNo: number) =>
@@ -36,4 +40,57 @@ export const getCommunityArticleReplyList = async ({
   await getAPI<paginationType<replyType>>(
     `/v1/communities/${commentNo}/replies`,
     { lastNo }
+  );
+// 댓글 작성
+export const postCommunityArticleComment = async (
+  comment: Pick<communityArticleDetailType, "communityPostNo"> &
+    Pick<baseContentType, "content">
+) =>
+  await postAPI<paginationType<commentType>>(
+    "/v1/communities/comments",
+    comment,
+    {
+      withCredentials: true
+    }
+  );
+// 답글 작성
+export const postCommunityArticleReply = async (
+  reply: Pick<commentType, "commentNo"> & Pick<baseContentType, "content">
+) =>
+  await postAPI<paginationType<replyType>>("/v1/communities/replies", reply, {
+    withCredentials: true
+  });
+// 댓글 삭제
+export const deleteCommunityArticleComment = async (commentNo: number) =>
+  await deleteAPI(`v1/communities/comments/${commentNo}`, {
+    withCredentials: true
+  });
+// 답글 삭제
+export const deleteCommunityArticleReply = async (replyNo: number) =>
+  await deleteAPI(`v1/communities/replies/${replyNo}`, {
+    withCredentials: true
+  });
+//댓글 수정
+export const putEditCommunityArticleComment = async ({
+  commentNo,
+  content
+}: Pick<commentType, "commentNo" | "content">) =>
+  await putAPI(
+    `v1/communities/comments/${commentNo}`,
+    { content },
+    {
+      withCredentials: true
+    }
+  );
+//답글 수정
+export const putEditCommunityArticleReply = async ({
+  replyNo,
+  content
+}: Pick<replyType, "replyNo" | "content">) =>
+  await putAPI(
+    `v1/communities/replies/${replyNo}`,
+    { content },
+    {
+      withCredentials: true
+    }
   );
