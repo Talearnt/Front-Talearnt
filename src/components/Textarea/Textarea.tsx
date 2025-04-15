f648ddd467ec7a48af442b7d785bccdfbc2895ba
ChoiBooKi <qnrl9034@naver.com>
2025-04-15 22:28:47 +0900

    [FEAT] Textarea wrapper 클릭 시 focus 되도록 | Focus textarea on wrapper click TALEARNT-402

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,9 +1,23 @@
-import { ChangeEvent, ComponentProps, forwardRef } from "react";
+import {
+  ChangeEvent,
+  ComponentProps,
+  forwardRef,
+  useImperativeHandle,
+  useRef
+} from "react";
 
 import { classNames } from "@utils/classNames";
 
 const Textarea = forwardRef<HTMLTextAreaElement, ComponentProps<"textarea">>(
   ({ className, onChange, maxLength, value, ...props }, ref) => {
+    const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+    // 외부 ref 연결
+    useImperativeHandle(ref, () => textareaRef.current as HTMLTextAreaElement);
+
+    // wrapper 클릭했을 때 textarea focus
+    const handleWrapperClick = () => textareaRef.current?.focus();
+    // textarea 값이 바뀔 때 높이를 변경
     const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
       const textarea = e.target;
       // 높이 자동 조절
@@ -17,12 +31,14 @@ const Textarea = forwardRef<HTMLTextAreaElement, ComponentProps<"textarea">>(
         className={classNames(
           "flex",
           "w-full rounded-lg border border-talearnt_Line_01 bg-talearnt_BG_Background px-[15px] py-[11px]",
+          "cursor-text",
           "focus-within:border-talearnt_Primary_01 hover:border-talearnt_Primary_01",
           className
         )}
+        onClick={handleWrapperClick}
       >
         <textarea
-          ref={ref}
+          ref={textareaRef}
           rows={1}
           className={classNames(
             "flex-1",
