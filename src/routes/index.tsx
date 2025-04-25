2d886074e5b4414c7cf2ae22a2b6e6c0dd06cf7f
ChoiBooKi <qnrl9034@naver.com>
2025-04-25 22:21:47 +0900

    [FEAT] 메인페이지 라우터 적용 | Apply MainPage router TALEARNT-416

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,21 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 import MainLayout from "@layout/MainLayout/MainLayout";
 import articlesRouter from "@routes/articles";
 import authRouter from "@routes/auth";
 
+const MainPage = lazy(() => import("@pages/MainPage/MainPage"));
+
 const router = createBrowserRouter([
   {
     element: <MainLayout />,
     path: "/",
-    children: [...authRouter, ...articlesRouter]
+    children: [
+      { element: <MainPage />, index: true },
+      ...authRouter,
+      ...articlesRouter
+    ]
   }
 ]);
 
