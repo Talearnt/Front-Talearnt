cbcef1897f56dfe044548b750a5694195c70f47c
ChoiBooKi <qnrl9034@naver.com>
2025-04-26 21:58:47 +0900

    [STYLE] 메인 레이아웃 수정 | Edit MainLayout.tsx TALEARNT-416

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -103,83 +103,103 @@ function MainLayout() {
   return (
     <>
       <header
-        className={classNames(
-          "flex items-center justify-between",
-          "h-[90px] border-b border-talearnt_Line_01 px-[80px]"
-        )}
+        className={classNames("h-[90px] border-b border-talearnt_Line_01")}
       >
-        <LogoIcon className={"cursor-pointer"} onClick={() => navigator("/")} />
-        <div className={"flex items-center gap-6"}>
-          <div className={"flex gap-2"}>
-            {linkArray.map(({ path, content }) => {
-              if (content === "회원가입" && isLoggedIn) {
-                return;
-              }
-
-              return (
-                <NavLink
-                  className={({ isActive }) =>
-                    classNames(
-                      "px-4 py-2",
-                      "text-body2_16_semibold text-talearnt_Text_02",
-                      isActive && "text-talearnt_Primary_01"
-                    )
-                  }
-                  to={path}
-                  key={path}
-                >
-                  {content}
-                </NavLink>
-              );
-            })}
-            <Button
-              buttonStyle={"outlined"}
-              className={classNames("w-[100px]", "text-body2_16_semibold")}
-              size={"small"}
-              onClick={() =>
-                navigator(
-                  isLoggedIn
-                    ? `write-article/${pathname.includes("community") ? "community" : "matching"}`
-                    : "sign-in"
-                )
-              }
-            >
-              {isLoggedIn ? "글쓰기" : "로그인"}
-            </Button>
-          </div>
-          {isLoggedIn && (
-            <>
-              <NotificationIcon />
-              <div className={"flex items-center"}>
-                <div
-                  className={
-                    "h-8 w-8 rounded-full border border-talearnt_Icon_02 bg-talearnt_BG_Up_02"
-                  }
-                />
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M4.60482 5.69531H11.3952C11.5153 5.69582 11.6326 5.73193 11.7322 5.79908C11.8318 5.86623 11.9092 5.96141 11.9547 6.07258C12.0002 6.18374 12.0118 6.30591 11.9878 6.42362C11.9639 6.54134 11.9056 6.64932 11.8203 6.73391L8.43123 10.123C8.37477 10.1799 8.30759 10.2251 8.23358 10.256C8.15957 10.2868 8.08018 10.3027 8 10.3027C7.91982 10.3027 7.84043 10.2868 7.76642 10.256C7.69241 10.2251 7.62523 10.1799 7.56877 10.123L4.17966 6.73391C4.09438 6.64932 4.03609 6.54134 4.01217 6.42362C3.98824 6.30591 3.99977 6.18374 4.04527 6.07258C4.09078 5.96141 4.16823 5.86623 4.26783 5.79908C4.36743 5.73193 4.4847 5.69582 4.60482 5.69531Z"
-                    fill="#414A4E"
-                  />
-                </svg>
-              </div>
-            </>
+        <div
+          className={classNames(
+            "flex items-center justify-between",
+            "mx-auto h-full w-[1440px] px-[80px]"
           )}
+        >
+          <LogoIcon
+            className={"cursor-pointer"}
+            onClick={() => navigator("/")}
+          />
+          <div className={"flex items-center gap-6"}>
+            <div className={"flex gap-2"}>
+              {linkArray.map(({ path, content }) => {
+                if (content === "회원가입" && isLoggedIn) {
+                  return;
+                }
+
+                return (
+                  <NavLink
+                    className={({ isActive }) =>
+                      classNames(
+                        "px-4 py-2",
+                        "text-body2_16_semibold text-talearnt_Text_02",
+                        isActive && "text-talearnt_Primary_01"
+                      )
+                    }
+                    to={path}
+                    key={path}
+                  >
+                    {content}
+                  </NavLink>
+                );
+              })}
+              <Button
+                buttonStyle={"outlined"}
+                className={classNames("w-[100px]", "text-body2_16_semibold")}
+                size={"small"}
+                onClick={() =>
+                  navigator(
+                    isLoggedIn
+                      ? `write-article/${pathname.includes("community") ? "community" : "matching"}`
+                      : "sign-in"
+                  )
+                }
+              >
+                {isLoggedIn ? "글쓰기" : "로그인"}
+              </Button>
+            </div>
+            {isLoggedIn && (
+              <>
+                <NotificationIcon />
+                <div className={"flex items-center"}>
+                  <div
+                    className={
+                      "h-8 w-8 rounded-full border border-talearnt_Icon_02 bg-talearnt_BG_Up_02"
+                    }
+                  />
+                  <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.60482 5.69531H11.3952C11.5153 5.69582 11.6326 5.73193 11.7322 5.79908C11.8318 5.86623 11.9092 5.96141 11.9547 6.07258C12.0002 6.18374 12.0118 6.30591 11.9878 6.42362C11.9639 6.54134 11.9056 6.64932 11.8203 6.73391L8.43123 10.123C8.37477 10.1799 8.30759 10.2251 8.23358 10.256C8.15957 10.2868 8.08018 10.3027 8 10.3027C7.91982 10.3027 7.84043 10.2868 7.76642 10.256C7.69241 10.2251 7.62523 10.1799 7.56877 10.123L4.17966 6.73391C4.09438 6.64932 4.03609 6.54134 4.01217 6.42362C3.98824 6.30591 3.99977 6.18374 4.04527 6.07258C4.09078 5.96141 4.16823 5.86623 4.26783 5.79908C4.36743 5.73193 4.4847 5.69582 4.60482 5.69531Z"
+                      fill="#414A4E"
+                    />
+                  </svg>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </header>
-      <main
+      <div
         ref={scrollRef}
-        className={"h-[calc(100vh-90px)] overflow-y-auto"}
+        className={classNames(
+          "flex flex-col",
+          "h-[calc(100vh-90px)] overflow-y-auto"
+        )}
         onScroll={handleScroll}
       >
-        <Outlet />
-      </main>
+        <main className={classNames("flex-1", "mx-auto w-[1440px] px-20")}>
+          <Outlet />
+        </main>
+        <footer
+          className={classNames(
+            "flex shrink-0 items-center justify-between",
+            "mt-[120px] h-[88px] border-t border-talearnt_Line_01 px-20"
+          )}
+        >
+          푸터
+        </footer>
+      </div>
       <TopButton topButtonRef={topButtonRef} />
       <Toast />
       {promptData && <Prompt />}
