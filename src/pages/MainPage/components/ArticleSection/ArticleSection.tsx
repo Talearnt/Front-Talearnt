f3828a1e27d67b73dbb3cef364417b23cfe2cdad
ChoiBooKi <qnrl9034@naver.com>
2025-05-19 22:10:00 +0900

    [FEAT] MoveButton 적용 | Apply MoveButton  TALEARNT-416

--- src/pages/MainPage/components/ArticleSection/ArticleSection.tsx
+++ src/pages/MainPage/components/ArticleSection/ArticleSection.tsx
@@ -5,6 +5,8 @@ import { classNames } from "@utils/classNames";
 
 import { useCarousel } from "@hook/useCarousel";
 
+import { MoveButton } from "@pages/MainPage/components/MoveButton/MoveButton";
+
 import { CaretIcon } from "@components/icons/caret/CaretIcon/CaretIcon";
 
 interface ArticleSectionProps {
@@ -86,23 +88,10 @@ export function ArticleSection({
           ))}
         </div>
       </div>
-      <button
-        className={classNames(
-          "flex items-center gap-4",
-          "mx-auto h-[50px] w-fit rounded-full bg-talearnt_BG_Up_01 px-4"
-        )}
+      <MoveButton
         onClick={() => navigator(articleType)}
-      >
-        <span className={"text-body2_16_semibold text-talearnt_Text_Strong"}>
-          {articleType === "community" ? "커뮤니티" : "매칭"} 게시물 더 보기
-        </span>
-        <CaretIcon
-          className={
-            "rounded-full bg-talearnt_Primary_01 stroke-talearnt_On_Icon"
-          }
-          size={30}
-        />
-      </button>
+        text={`${articleType === "community" ? "커뮤니티" : "매칭"} 게시물 더 보기`}
+      />
     </div>
   );
 }
