c6d49a96c0a178ac3c2ed0abf5169ab4c830252e
ChoiBooKi <qnrl9034@naver.com>
2025-05-21 21:21:00 +0900

    [FEAT] NoticeCard.tsx 개발 | Make NoticeCard.tsx TALEARNT-416

---
+++ src/components/NoticeCard/NoticeCard.tsx
@@ -0,0 +1,58 @@
+import dayjs from "dayjs";
+
+import { classNames } from "@utils/classNames";
+
+import { Badge } from "@components/Badge/Badge";
+
+type NoticeCardProps = {
+  noticeType: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  pageType?: "main" | "notice";
+};
+
+function NoticeCard({
+  noticeType,
+  title,
+  content,
+  createdAt,
+  pageType = "main"
+}: NoticeCardProps) {
+  return (
+    <div
+      className={classNames(
+        "flex flex-col gap-2",
+        "rounded-2xl border border-talearnt_Line_01 p-[23px]"
+      )}
+    >
+      <div className={"flex gap-1"}>
+        <Badge label={noticeType} />
+      </div>
+      <h2
+        className={classNames(
+          "text-heading4_20_semibold text-talearnt_Text_Strong",
+          pageType === "main" && "line-clamp-2 h-[52px]",
+          pageType === "notice" && "line-clamp-1"
+        )}
+      >
+        {title}
+      </h2>
+      <p
+        className={classNames(
+          "mb-3",
+          "text-body3_14_medium text-talearnt_Text_03",
+          pageType === "main" && "line-clamp-2 h-[36.4px]",
+          pageType === "notice" && "line-clamp-1"
+        )}
+      >
+        {content}
+      </p>
+      <span className={"text-caption1_14_medium text-talearnt_Text_03"}>
+        {dayjs(createdAt).format("YYYY.MM.DD")}
+      </span>
+    </div>
+  );
+}
+
+export { NoticeCard };
