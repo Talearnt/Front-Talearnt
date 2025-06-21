03486a4c7a7671ab8d1d21d366272012aff109c4
ChoiBooKi <qnrl9034@naver.com>
2025-06-21 22:44:47 +0900

    [FEAT] useDebounce.ts undefined return 추가 | Add undefined return to useDebounce.ts TALEARNT-476

--- src/shared/hooks/useDebounce.ts
+++ src/shared/hooks/useDebounce.ts
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
 
-const useDebounce = <T = string>(value: T, delay = 300) => {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+const useDebounce = <T = string>(
+  value: T,
+  {
+    delay = 300,
+    returnUndefinedBeforeDebounce = false,
+  }: { delay?: number; returnUndefinedBeforeDebounce?: boolean } = {}
+) => {
+  const [debouncedValue, setDebouncedValue] = useState<T | undefined>(value);
 
   useEffect(() => {
     // 타이머가 시작됩니다.
@@ -11,8 +17,13 @@ const useDebounce = <T = string>(value: T, delay = 300) => {
 
     // 컴포넌트가 언마운트되거나 value 또는 delay가 변경될 때
     // 이전 타이머를 정리하고 새로운 타이머를 설정합니다.
-    return () => clearTimeout(handler);
-  }, [value, delay]); // value나 delay가 변경될 때마다 실행됩니다.
+    return () => {
+      clearTimeout(handler);
+      if (returnUndefinedBeforeDebounce) {
+        setDebouncedValue(undefined);
+      }
+    };
+  }, [value, delay, returnUndefinedBeforeDebounce]); // value나 delay가 변경될 때마다 실행됩니다.
 
   return debouncedValue; // 디바운싱된 값을 반환합니다.
 };
