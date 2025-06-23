0266dc711f550cf3cd2011a2e19a0201c297abfd
ChoiBooKi <qnrl9034@naver.com>
2025-06-23 22:08:47 +0900

    [FEAT] 프로필 수정 API, hook 개발 | Make edit profile API, hook TALEARNT-476

---
+++ src/features/user/profile/profile.hook.ts
@@ -0,0 +1,69 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { postToGetPresignedURL } from "@features/articles/shared/writeArticle.api";
+import { putProfile } from "@features/user/profile/profile.api";
+
+import { createQueryKey } from "@shared/utils/createQueryKey";
+
+import { queryKeys } from "@shared/constants/queryKeys";
+
+import { profileType } from "@features/user/user.type";
+
+// 프로필 수정
+export const usePutProfile = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({
+      file,
+      ...data
+    }: Omit<profileType, "profileImg" | "userId" | "userNo"> & {
+      file: File | null;
+    }) => {
+      let profileImg = null;
+
+      if (file) {
+        const { data: presigned } = await postToGetPresignedURL([
+          { fileName: file.name, fileType: file.type, fileSize: file.size },
+        ]);
+
+        await fetch(presigned[0], {
+          method: "PUT",
+          body: file,
+          headers: new Headers({ "Content-Type": file.type }),
+        });
+
+        const { origin, pathname } = new URL(presigned[0]);
+
+        profileImg = origin + pathname;
+      }
+
+      return await putProfile({ ...data, profileImg });
+    },
+    onSuccess: async data => {
+      await queryClient.invalidateQueries({
+        queryKey: createQueryKey([queryKeys.COMMUNITY], { isList: true }),
+      });
+      await queryClient.invalidateQueries({
+        queryKey: createQueryKey([queryKeys.MATCHING], { isList: true }),
+      });
+      await queryClient.invalidateQueries({
+        queryKey: createQueryKey([queryKeys.COMMUNITY_COMMENT], {
+          isList: true,
+        }),
+      });
+      await queryClient.invalidateQueries({
+        queryKey: createQueryKey([queryKeys.COMMUNITY_REPLY], {
+          isList: true,
+        }),
+      });
+
+      queryClient.setQueryData(
+        createQueryKey([queryKeys.USER, "profile"], {
+          isLoggedIn: true,
+        }),
+        data
+      );
+    },
+  });
+};
