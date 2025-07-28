b20ce7abb4c73b52fe5219963ead6f4560aaa9a4
ChoiBooKi <qnrl9034@naver.com>
2025-07-28 23:15:00 +0900

    [FEAT] 찜 목록 리스트 없을 때 | Apply no data TALEARNT-479

--- src/pages/user/FavoriteMatchingArticleList.tsx
+++ src/pages/user/FavoriteMatchingArticleList.tsx
@@ -1,13 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 import { classNames } from "@shared/utils/classNames";
 
 import { useGetFavoriteMatchingArticleList } from "@features/user/favoriteMatchingArticleList/favoriteMatchingArticleList.hook";
 
 import { useFavoriteMatchingArticlePageStore } from "@features/user/favoriteMatchingArticleList/favoriteMatchingArticleList.store";
 
+import { EmptyState } from "@components/common/EmptyState/EmptyState";
 import { Pagination } from "@components/common/Pagination/Pagination";
 import { MatchingArticleCard } from "@components/shared/MatchingArticleCard/MatchingArticleCard";
 
 function FavoriteMatchingArticleList() {
+  const navigator = useNavigate();
+
   const {
     data: {
       data: {
@@ -20,39 +25,55 @@ function FavoriteMatchingArticleList() {
   const { page, setPage } = useFavoriteMatchingArticlePageStore();
 
   return (
-    <div className={"flex flex-col"}>
-      <div className={classNames("flex items-center", "mb-4 h-10")}>
-        <span className={"text-heading3_22_semibold text-talearnt_Text_02"}>
-          총
-        </span>
-        <span
-          className={classNames(
-            "ml-1",
-            "text-heading2_24_semibold text-talearnt_Primary_01"
-          )}
-        >
-          {totalCount}
-        </span>
-        <span className={"text-heading3_22_semibold text-talearnt_Text_02"}>
-          개의 매칭 게시물을 찜했어요
-        </span>
-      </div>
-      <div className={"grid grid-cols-[repeat(3,305px)] gap-5"}>
-        {/*TODO 리스트 없을 때*/}
-        {results.map(({ exchangePostNo, ...article }) => (
-          <MatchingArticleCard
-            {...article}
-            exchangePostNo={exchangePostNo}
-            key={exchangePostNo}
+    <div
+      className={classNames(
+        "flex flex-col",
+        results.length === 0 &&
+          "h-[756px] justify-center rounded-[20px] border border-talearnt_Line_01"
+      )}
+    >
+      {results.length === 0 ? (
+        <EmptyState
+          title={"아직 찜한 게시물이 없어요"}
+          description={"마음에 드는 게시물을 찜해보세요!"}
+          buttonText={"게시물 찜하러 가기"}
+          buttonOnClick={() => navigator("/matching")}
+        />
+      ) : (
+        <>
+          <div className={classNames("flex items-center", "mb-4 h-10")}>
+            <span className={"text-heading3_22_semibold text-talearnt_Text_02"}>
+              총
+            </span>
+            <span
+              className={classNames(
+                "ml-1",
+                "text-heading2_24_semibold text-talearnt_Primary_01"
+              )}
+            >
+              {totalCount}
+            </span>
+            <span className={"text-heading3_22_semibold text-talearnt_Text_02"}>
+              개의 매칭 게시물을 찜했어요
+            </span>
+          </div>
+          <div className={"grid grid-cols-[repeat(3,305px)] gap-5"}>
+            {results.map(({ exchangePostNo, ...article }) => (
+              <MatchingArticleCard
+                {...article}
+                exchangePostNo={exchangePostNo}
+                key={exchangePostNo}
+              />
+            ))}
+          </div>
+          <Pagination
+            className={"mt-14"}
+            currentPage={page}
+            totalPages={totalPages}
+            handlePageChange={setPage}
           />
-        ))}
-      </div>
-      <Pagination
-        className={"mt-14"}
-        currentPage={page}
-        totalPages={totalPages}
-        handlePageChange={setPage}
-      />
+        </>
+      )}
     </div>
   );
 }
