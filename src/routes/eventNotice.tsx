c5893bc2fbbb81d593d8e88c99bda6d836f38ff5
ChoiBooKi <qnrl9034@naver.com>
2025-08-29 22:01:00 +0900

    [FEAT] 이벤트, 공지사항 상세페이지 추가 | Add event, notice detail page route TALEARNT-491

--- src/routes/eventNotice.tsx
+++ src/routes/eventNotice.tsx
@@ -1,16 +1,36 @@
 import { lazy, Suspense } from "react";
 import { RouteObject } from "react-router-dom";
 
-const EventNoticePage = lazy(() => import("@pages/EventNoticePage"));
+const EventNoticeList = lazy(
+  () => import("@pages/eventNotice/EventNoticeList")
+);
+const EventDetail = lazy(() => import("@pages/eventNotice/EventDetail"));
+const NoticeDetail = lazy(() => import("@pages/eventNotice/NoticeDetail"));
 
 const eventNoticeRouter: RouteObject[] = [
   {
     element: (
       <Suspense>
-        <EventNoticePage />
+        <EventNoticeList />
       </Suspense>
     ),
     path: "event-notice/:tab",
+  },
+  {
+    element: (
+      <Suspense>
+        <EventDetail />
+      </Suspense>
+    ),
+    path: "event-notice/event/:eventNo",
+  },
+  {
+    element: (
+      <Suspense>
+        <NoticeDetail />
+      </Suspense>
+    ),
+    path: "event-notice/notice/:noticeNo",
   },
 ];
 
