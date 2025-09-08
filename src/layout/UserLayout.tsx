d3d6beef3c8e694a624195555bf1acfe37ccb1ad
ChoiBooKi <qnrl9034@naver.com>
2025-09-08 21:06:59 +0900

    [FEAT] 문의하기 적용 | Apply inquiry TALEARNT-491

--- src/layout/UserLayout.tsx
+++ src/layout/UserLayout.tsx
@@ -2,6 +2,8 @@ import { NavLink, Outlet } from "react-router-dom";
 
 import { classNames } from "@shared/utils/classNames";
 
+import { HeadsetIcon } from "@components/common/icons/styled/HeadsetIcon";
+
 import { userSidebarArray } from "@features/user/user.constants";
 
 function UserLayout() {
@@ -38,6 +40,31 @@ function UserLayout() {
             </div>
           </div>
         ))}
+        <div className={"flex flex-col gap-3"}>
+          <div className={"px-4 py-2"}>
+            <span className={"text-heading4_20_semibold text-talearnt_Text_01"}>
+              기타
+            </span>
+          </div>
+          <div className={"flex flex-col gap-2"}>
+            <button
+              className={classNames(
+                "flex items-center gap-3",
+                "rounded-lg px-4 py-[15px]",
+                "hover:bg-talearnt_BG_Up_01"
+              )}
+              onClick={() =>
+                (window.location.href =
+                  "mailto:woong9421@nate.com?subject=제목&body=내용")
+              }
+            >
+              <HeadsetIcon />
+              <span className={`text-body1_18_semibold text-talearnt_Text_02`}>
+                문의하기
+              </span>
+            </button>
+          </div>
+        </div>
       </div>
       <Outlet />
     </div>
