310d7d4134d1d72698e80941948b57b886453d91
ChoiBooKi <qnrl9034@naver.com>
2025-09-20 21:37:59 +0900

    [FIX] 일반적인 이메일 정규식으로 변경 | Changed to a general email regex TALEARNT-491

--- src/features/auth/shared/authRegex.constants.ts
+++ src/features/auth/shared/authRegex.constants.ts
@@ -1,6 +1,5 @@
 // 정규식
-export const userIdRegex =
-  /^$|^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/;
+export const userIdRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 export const pwRegex =
   /^$|^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?~-]).{8,}$/;
