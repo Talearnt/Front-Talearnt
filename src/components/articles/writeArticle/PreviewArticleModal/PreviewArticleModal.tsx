aa73288192452a03d7ff735cfaf16780cfb3923e
ChoiBooKi <qnrl9034@naver.com>
2025-10-28 22:10:59 +0900

    [STYLE] 스크롤바 여백 수정 | Modify scrollbar padding TALEARNT-491

--- src/components/articles/writeArticle/PreviewArticleModal/PreviewArticleModal.tsx
+++ src/components/articles/writeArticle/PreviewArticleModal/PreviewArticleModal.tsx
@@ -63,158 +63,155 @@ function PreviewArticleModal({
         <ModalBody
           bodyRef={scrollRef}
           className={classNames(
-            "max-h-[600px] min-h-[400px]",
+            "flex flex-col gap-6",
+            "max-h-[600px] min-h-[400px] pb-6 pl-8",
             "scrollbar scrollbar-w12-10 overflow-y-auto"
           )}
         >
-          <div className={classNames("flex flex-col gap-6", "px-8 pb-6")}>
-            <h1
-              className={"text-heading1_30_semibold text-talearnt_Text_Strong"}
-            >
-              {title}
-            </h1>
-            <div className={"flex items-center gap-4"}>
-              <Avatar imageUrl={profileImg} />
-              <span className={"text-body1_18_semibold text-talearnt_Text_01"}>
-                {nickname}
-              </span>
-              <div className={"h-5 w-px bg-talearnt_Line_01"} />
-              <span className={"text-body1_18_semibold text-talearnt_Text_04"}>
-                {dayjs().format("YYYY-MM-DD")}
-              </span>
-              <Badge
-                label={
-                  type === "matching"
-                    ? "모집중"
-                    : (data as communityArticlePreviewProps).postType
-                }
-                size={"medium"}
-              />
-            </div>
-            {type === "matching" && (
-              <>
-                <div className={"grid grid-cols-2 gap-6"}>
-                  <div className={"flex flex-col gap-2"}>
-                    <label
-                      className={"text-body2_16_semibold text-talearnt_Text_03"}
-                    >
-                      주고 싶은 나의 재능
-                    </label>
-                    <div className={"flex flex-wrap gap-2"}>
-                      {findTalentList(
-                        (data as matchingArticlePreviewProps).giveTalents
-                      ).map(({ talentCode, talentName }) => (
-                        <Badge
-                          label={talentName}
-                          color={"darkgray"}
-                          rounded={"md"}
-                          size={"medium"}
-                          key={`preview-giveTalent-${talentCode}`}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                  <div className={"flex flex-col gap-2"}>
-                    <label
-                      className={"text-body2_16_semibold text-talearnt_Text_03"}
-                    >
-                      받고 싶은 상대의 재능
-                    </label>
-                    <div className={"flex flex-wrap gap-2"}>
-                      {findTalentList(
-                        (data as matchingArticlePreviewProps).receiveTalents
-                      ).map(({ talentCode, talentName }) => (
-                        <Badge
-                          label={talentName}
-                          color={"darkgray"}
-                          rounded={"md"}
-                          size={"medium"}
-                          key={`preview-receiveTalent-${talentCode}`}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                </div>
-                <div className={"flex gap-16"}>
-                  <div className={"flex gap-4"}>
-                    <label
-                      className={"text-body2_16_semibold text-talearnt_Text_03"}
-                    >
-                      진행 방식
-                    </label>
-                    <span
-                      className={"text-body2_16_semibold text-talearnt_Text_02"}
-                    >
-                      {(data as matchingArticlePreviewProps).exchangeType}
-                    </span>
-                  </div>
-                  <div className={"flex gap-4"}>
-                    <label
-                      className={"text-body2_16_semibold text-talearnt_Text_03"}
-                    >
-                      진행 기간
-                    </label>
-                    <span
-                      className={"text-body2_16_semibold text-talearnt_Text_02"}
-                    >
-                      {(data as matchingArticlePreviewProps).duration}
-                    </span>
-                  </div>
-                </div>
-              </>
-            )}
-            {imageUrls.length > 0 && (
-              <div
-                className={classNames(
-                  "flex gap-4",
-                  "rounded-2xl bg-talearnt_BG_Up_02 p-6"
-                )}
-              >
-                {imageUrls.map((url, index) => {
-                  if (index > 3) {
-                    return null;
-                  }
-
-                  return (
-                    <div
-                      className={classNames(
-                        "relative",
-                        "h-[152px] w-[152px] cursor-pointer overflow-hidden rounded-2xl"
-                      )}
-                      key={url}
-                    >
-                      <img
-                        className={"h-full w-full object-cover"}
-                        src={url}
-                        alt={`${index}번째 업로드 이미지`}
-                      />
-                      {imageUrls.length > 4 && (
-                        <div
-                          className={classNames(
-                            "absolute top-0",
-                            "flex items-center justify-center",
-                            "h-full w-full bg-black/60"
-                          )}
-                        >
-                          <span
-                            className={
-                              "text-body2_16_semibold text-talearnt_BG_Background"
-                            }
-                          >
-                            이미지
-                            <br />
-                            더보기
-                          </span>
-                        </div>
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-            <div className={"h-px w-full bg-talearnt_Line_01"} />
-            <p dangerouslySetInnerHTML={{ __html: content }} />
+          <h1 className={"text-heading1_30_semibold text-talearnt_Text_Strong"}>
+            {title}
+          </h1>
+          <div className={"flex items-center gap-4"}>
+            <Avatar imageUrl={profileImg} />
+            <span className={"text-body1_18_semibold text-talearnt_Text_01"}>
+              {nickname}
+            </span>
+            <div className={"h-5 w-px bg-talearnt_Line_01"} />
+            <span className={"text-body1_18_semibold text-talearnt_Text_04"}>
+              {dayjs().format("YYYY-MM-DD")}
+            </span>
+            <Badge
+              label={
+                type === "matching"
+                  ? "모집중"
+                  : (data as communityArticlePreviewProps).postType
+              }
+              size={"medium"}
+            />
           </div>
+          {type === "matching" && (
+            <>
+              <div className={"grid grid-cols-2 gap-6"}>
+                <div className={"flex flex-col gap-2"}>
+                  <label
+                    className={"text-body2_16_semibold text-talearnt_Text_03"}
+                  >
+                    주고 싶은 나의 재능
+                  </label>
+                  <div className={"flex flex-wrap gap-2"}>
+                    {findTalentList(
+                      (data as matchingArticlePreviewProps).giveTalents
+                    ).map(({ talentCode, talentName }) => (
+                      <Badge
+                        label={talentName}
+                        color={"darkgray"}
+                        rounded={"md"}
+                        size={"medium"}
+                        key={`preview-giveTalent-${talentCode}`}
+                      />
+                    ))}
+                  </div>
+                </div>
+                <div className={"flex flex-col gap-2"}>
+                  <label
+                    className={"text-body2_16_semibold text-talearnt_Text_03"}
+                  >
+                    받고 싶은 상대의 재능
+                  </label>
+                  <div className={"flex flex-wrap gap-2"}>
+                    {findTalentList(
+                      (data as matchingArticlePreviewProps).receiveTalents
+                    ).map(({ talentCode, talentName }) => (
+                      <Badge
+                        label={talentName}
+                        color={"darkgray"}
+                        rounded={"md"}
+                        size={"medium"}
+                        key={`preview-receiveTalent-${talentCode}`}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className={"flex gap-16"}>
+                <div className={"flex gap-4"}>
+                  <label
+                    className={"text-body2_16_semibold text-talearnt_Text_03"}
+                  >
+                    진행 방식
+                  </label>
+                  <span
+                    className={"text-body2_16_semibold text-talearnt_Text_02"}
+                  >
+                    {(data as matchingArticlePreviewProps).exchangeType}
+                  </span>
+                </div>
+                <div className={"flex gap-4"}>
+                  <label
+                    className={"text-body2_16_semibold text-talearnt_Text_03"}
+                  >
+                    진행 기간
+                  </label>
+                  <span
+                    className={"text-body2_16_semibold text-talearnt_Text_02"}
+                  >
+                    {(data as matchingArticlePreviewProps).duration}
+                  </span>
+                </div>
+              </div>
+            </>
+          )}
+          {imageUrls.length > 0 && (
+            <div
+              className={classNames(
+                "flex gap-4",
+                "rounded-2xl bg-talearnt_BG_Up_02 p-6"
+              )}
+            >
+              {imageUrls.map((url, index) => {
+                if (index > 3) {
+                  return null;
+                }
+
+                return (
+                  <div
+                    className={classNames(
+                      "relative",
+                      "h-[152px] w-[152px] cursor-pointer overflow-hidden rounded-2xl"
+                    )}
+                    key={url}
+                  >
+                    <img
+                      className={"h-full w-full object-cover"}
+                      src={url}
+                      alt={`${index}번째 업로드 이미지`}
+                    />
+                    {imageUrls.length > 4 && (
+                      <div
+                        className={classNames(
+                          "absolute top-0",
+                          "flex items-center justify-center",
+                          "h-full w-full bg-black/60"
+                        )}
+                      >
+                        <span
+                          className={
+                            "text-body2_16_semibold text-talearnt_BG_Background"
+                          }
+                        >
+                          이미지
+                          <br />
+                          더보기
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+          <div className={"h-px min-h-px w-full bg-talearnt_Line_01"} />
+          <p dangerouslySetInnerHTML={{ __html: content }} />
         </ModalBody>
         <ModalBottom className={"justify-center"}>
           <Button onClick={onCloseHandler}>확인</Button>
