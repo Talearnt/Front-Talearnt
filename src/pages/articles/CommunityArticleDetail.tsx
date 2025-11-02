5c1a1be6f0348261a8d263175dfd67d5324290ba
ChoiBooKi <qnrl9034@naver.com>
2025-11-02 22:00:59 +0900

    [STYLE] 아이콘과 동일한 width 로 border width 변경 | Adjust border width to match icon width TALEARNT-491

--- src/pages/articles/CommunityArticleDetail.tsx
+++ src/pages/articles/CommunityArticleDetail.tsx
@@ -187,7 +187,7 @@ function CommunityArticleDetail() {
             <button
               className={classNames(
                 "flex items-center gap-1",
-                "ml-auto rounded-lg p-2 shadow-[inset_0_0_0_1px] shadow-talearnt_Line_01",
+                "ml-auto rounded-lg p-2 shadow-[inset_0_0_0_1.4px] shadow-talearnt_Line_01",
                 "cursor-pointer",
                 "hover:bg-talearnt_BG_Up_01"
               )}
